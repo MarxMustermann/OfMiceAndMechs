bc51511eeccb3cb73f56c5c6df473d44af777352
MarxMustermann <invalid@example.com>
2017-09-08 03:02:06 +0200

    added crude multi room support

--- test.py
+++ test.py
@@ -41,8 +41,135 @@ def calculatePath(startX,startY,endX,endY):
 					diffY-1
 				path.append((startX,startY))
 			"""
-	#path = path[:-1]
 	return path
+
+class Room(object):
+	def __init__(self,layout):
+		self.layout = layout
+		self.itemsOnFloor = []
+		self.characters = []
+
+		lineCounter = 0
+		for line in self.layout[1:].split("\n"):
+			rowCounter = 0
+			for char in line:
+				if char == "X":
+					self.itemsOnFloor.append(Wall(rowCounter,lineCounter))
+				if char == "$":
+					self.itemsOnFloor.append(Door(rowCounter,lineCounter))
+				rowCounter += 1
+			lineCounter += 1
+
+
+	def render(self):
+		chars = []
+		for i in range(0,10):
+			subChars = []
+			for j in range(0,10):
+				subChars.append(" ")
+			chars.append(subChars)
+
+		if len(characters[0].quests):
+			try:
+				chars[characters[0].quests[0].dstY][characters[0].quests[0].dstX] = "X"
+
+				path = calculatePath(characters[0].xPosition,characters[0].yPosition,characters[0].quests[0].dstX,characters[0].quests[0].dstY)
+				for item in path:
+					chars[item[1]][item[0]] = "x"
+			except:
+				pass
+		
+		for item in self.itemsOnFloor:
+			chars[item.yPosition][item.xPosition] = item.display
+
+		for character in self.characters:
+			chars[character.yPosition][character.xPosition] = character.display
+
+		lines = []
+		for lineChars in chars:
+			lines.append("".join(lineChars))
+
+		return "\n".join(lines)
+
+	def addCharacter(self,character,x,y):
+		self.characters.append(character)
+		character.room = self
+		character.xPosition = x
+		character.yPosition = y
+
+	def removeCharacter(self,character):
+		self.characters.remove(character)
+		character.room = None
+
+class Room1(Room):
+	def __init__(self):
+		self.roomLayout = """
+XXXXXXXXXX
+X        X
+X        X
+X        X
+X        X
+X        X
+X        X
+X        X
+X        X
+XXXX$XXXXX
+"""
+		super().__init__(self.roomLayout)
+
+class Room2(Room):
+	def __init__(self):
+		self.roomLayout = """
+XXXX$XXXXX
+X@Iv vID#X
+X@      #X
+X@ 8#OF PX
+X@ ##OF PX
+XB 8#OF PX
+XB |DI  PX
+XB      #X
+XPPPPPID#X
+XXXXXXXXXX
+"""
+		super().__init__(self.roomLayout)
+
+		self.lever1 = Lever(3,6,"engine control")
+		self.lever2 = Lever(1,2,"boarding alarm")
+
+		def lever2action(self):
+			messages.append("Bitte unterlassen Sie das anschalten des Alarms!")
+			deactivateLeaverQuest = ActivateQuest(self.lever2,desiredActive=False)
+			characters[1].assignQuest(deactivateLeaverQuest,active=True)
+
+		self.lever2.activateAction = lever2action
+
+		coalPile1 = Pile(8,3,"coal Pile1",Coal)
+		coalPile2 = Pile(8,4,"coal Pile2",Coal)
+		coalPile3 = Pile(8,5,"coal Pile3",Coal)
+		coalPile4 = Pile(8,6,"coal Pile4",Coal)
+		self.furnace = Furnace(6,6,"Furnace")
+		furnaceDisplay = Display(8,8,"Furnace monitoring")
+
+		self.itemsOnFloor.extend([self.lever1,self.lever2,coalPile1,coalPile2,coalPile3,coalPile4,self.furnace,furnaceDisplay])
+
+		quest0 = ActivateQuest(self.lever1)
+		quest1 = MoveQuest(2,2)
+		quest2 = MoveQuest(2,7)
+		quest3 = MoveQuest(7,7)
+		quest4 = MoveQuest(7,2)
+		quest0.followUp = quest1
+		quest1.followUp = quest2
+		quest2.followUp = quest3
+		quest3.followUp = quest4
+		quest4.followUp = quest1
+		npcQuests = [quest0]
+		npc = Character("Ö",2,1,npcQuests,name="Erwin von Libwig")
+		npc.watched = True
+
+		npc2 = Character("Ü",1,1,name="Ernst Ziegelbach")
+
+		self.addCharacter(npc,2,1)
+		self.addCharacter(npc2,1,1)
 
 class Item(object):
 	def __init__(self,display="§",xPosition=0,yPosition=0):
@@ -132,10 +259,16 @@ class Wall(Item):
 		super().__init__("X",xPosition,yPosition)
 
 class Coal(Item):
-	def __init__(self,xPosition=0,yPosition=0,name="coal"):
+	def __init__(self,xPosition=0,yPosition=0,name="Coal"):
 		self.name = name
 		self.canBurn = True
 		super().__init__("*",xPosition,yPosition)
+
+class Door(Item):
+	def __init__(self,xPosition=0,yPosition=0,name="Door"):
+		self.name = name
+		self.canBurn = True
+		super().__init__("$",xPosition,yPosition)
 
 class Pile(Item):
 	def __init__(self,xPosition=0,yPosition=0,name="pile",itemType=Coal):
@@ -160,6 +293,7 @@ class Character():
 		self.inventory = []
 		self.watched = False
 		self.listeners = []
+		self.room = None
 		
 		for quest in quests:
 			self.assignQuest(quest)
@@ -275,7 +409,8 @@ class CollectQuest(Quest):
 	def __init__(self,toFind="canBurn",startCinematics=None):
 		self.toFind = toFind
 		self.description = "please fetch things with property: "+toFind
-
+		
+		foundItem = None
 		for item in itemsOnFloor:
 			hasProperty = False
 			try:
@@ -371,14 +506,7 @@ class GameState():
 
 messages = []
 
-lever1 = Lever(3,6,"engine control")
-lever2 = Lever(1,2,"boarding alarm")
 
-
-def lever2action(self):
-	messages.append("Bitte unterlassen Sie das anschalten des Alarms!")
-	deactivateLeaverQuest = ActivateQuest(lever2,desiredActive=False)
-	characters[1].assignQuest(deactivateLeaverQuest,active=True)
 
 class Cinematic(object):
 	def __init__(self,text):
@@ -402,17 +530,7 @@ def showCinematic(text):
 
 showCinematic("welcome to the Trainingenvironment\n\nplease, try to learn fast.\n\nParticipants with low Evaluationscores will be given suitable Assignments in the Vats")
 
-lever2.activateAction = lever2action
-
-coalPile1 = Pile(8,3,"coal Pile1",Coal)
-coalPile2 = Pile(8,4,"coal Pile2",Coal)
-coalPile3 = Pile(8,5,"coal Pile3",Coal)
-coalPile4 = Pile(8,6,"coal Pile4",Coal)
-furnace = Furnace(6,6,"Furnace")
-furnaceDisplay = Display(8,8,"Furnace monitoring")
-
-
-itemsOnFloor = [lever1,lever2,furnace,furnaceDisplay,coalPile1,coalPile2,coalPile3,coalPile4]
+itemsOnFloor = []
 
 roomLayout = """
 XXXXXXXXXX
@@ -483,33 +601,20 @@ for line in roomLayout[1:].split("\n"):
 		rowCounter += 1
 	lineCounter += 1
 
+room1 = Room1()
+room2 = Room2()
 
-quest0 = ActivateQuest(lever1)
-quest1 = MoveQuest(2,2)
-quest2 = MoveQuest(2,7)
-quest3 = MoveQuest(7,7)
-quest4 = MoveQuest(7,2)
-quest0.followUp = quest1
-quest1.followUp = quest2
-quest2.followUp = quest3
-quest3.followUp = quest4
-quest4.followUp = quest1
-npcQuests = [quest0]
-npc = Character("Ö",2,1,npcQuests,name="Erwin von Libwig")
-npc.watched = True
-
-npcQuests = [quest0]
-npc2 = Character("Ü",1,1,name="Ernst Ziegelbach")
+rooms = [room1,room2]
 
 tutorialQuest1 = MoveQuest(5,5,startCinematics="inside the Simulationchamber everything has to be taught from Scratch\n\nthe basic Movementcommands are:\n\n w=up\n a=right\n s=down\n d=right\n\nplease move to the designated Target. the Implant will mark your Way")
 tutorialQuest2 = CollectQuest(startCinematics="interaction with your Environment ist somewhat complicated\n\nthe basic Interationcommands are:\n\n j=activate/apply\n e=examine\n k=pick up\n\nsee this Piles of Coal marked with ӫ on the rigth Side of the room.\n\nplease grab yourself some Coal from a pile by moving onto it and pressing j.")
-tutorialQuest3 = ActivateQuest(furnace,startCinematics="now go and activate the Furnace marked with a Ω. you need to have burnable Material like Coal in your Inventory\n\nso ensure that you have some Coal in your Inventory go to the Furnace and press j.")
+tutorialQuest3 = ActivateQuest(room2.furnace,startCinematics="now go and activate the Furnace marked with a Ω. you need to have burnable Material like Coal in your Inventory\n\nso ensure that you have some Coal in your Inventory go to the Furnace and press j.")
 tutorialQuest4 = MoveQuest(1,3,startCinematics="Move back to waiting position")
 quest0 = CollectQuest()
-quest05 = ActivateQuest(furnace)
+quest05 = ActivateQuest(room2.furnace)
 quest1 = MoveQuest(10,10)
 quest2 = MoveQuest(0,0)
-quest3 = ActivateQuest(lever2)
+quest3 = ActivateQuest(room2.lever2)
 tutorialQuest1.followUp = tutorialQuest2
 tutorialQuest2.followUp = tutorialQuest3
 tutorialQuest3.followUp = tutorialQuest4
@@ -522,8 +627,9 @@ quest3.followUp = None
 mainQuests = [tutorialQuest1]
 mainChar = Character("@",1,3,mainQuests,False,name="Sigmund Bärenstein")
 mainChar.watched = True
+room2.addCharacter(mainChar,1,3)
 
-characters = [mainChar,npc,npc2]
+characters = [mainChar]
 
 gamestate = GameState(characters)
 
@@ -552,9 +658,16 @@ def show_or_exit(key):
 	if key in ('w'):
 		if characters[0].yPosition:
 			characters[0].yPosition -= 1
+		else:
+			room1.addCharacter(mainChar,4,9)
+			room2.removeCharacter(mainChar)
 		characters[0].changed()
 	if key in ('s'):
-		characters[0].yPosition += 1
+		if characters[0].yPosition < 9:
+			characters[0].yPosition += 1
+		else:
+			room2.addCharacter(mainChar,4,0)
+			room1.removeCharacter(mainChar)
 		characters[0].changed()
 	if key in ('d'):
 		characters[0].xPosition += 1
@@ -564,7 +677,7 @@ def show_or_exit(key):
 			characters[0].xPosition -= 1
 		characters[0].changed()
 	if key in ('j'):
-		for item in itemsOnFloor:
+		for item in mainChar.room.itemsOnFloor:
 			if item.xPosition == characters[0].xPosition and item.yPosition == characters[0].yPosition:
 				item.apply()
 	if key in ('l'):
@@ -615,34 +728,11 @@ def renderMessagebox():
 	return txt
 
 def render():
-	chars = []
-	for i in range(1,20):
-		subChars = []
-		for j in range(1,20):
-			subChars.append(" ")
-		chars.append(subChars)
-
-	if len(characters[0].quests):
-		try:
-			chars[characters[0].quests[0].dstY][characters[0].quests[0].dstX] = "X"
-
-			path = calculatePath(characters[0].xPosition,characters[0].yPosition,characters[0].quests[0].dstX,characters[0].quests[0].dstY)
-			for item in path:
-				chars[item[1]][item[0]] = "x"
-		except:
-			pass
-	
-	for item in itemsOnFloor:
-		chars[item.yPosition][item.xPosition] = item.display
-
-	for character in characters:
-		chars[character.yPosition][character.xPosition] = character.display
-
-	lines = []
-	for lineChars in chars:
-		lines.append("".join(lineChars))
-
-	return "\n".join(lines)
+	result = ""
+	result += rooms[0].render()
+	result += "\n"
+	result += rooms[1].render()
+	return result
 
 frame = urwid.Frame(urwid.Filler(main,"top"),header=header,footer=footer)
 loop = urwid.MainLoop(frame, unhandled_input=show_or_exit)

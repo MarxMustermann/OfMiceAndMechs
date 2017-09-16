df027b2abdb6b64def77bb031adb8c313bb372f1
MarxMustermann <invalid@example.com>
2017-09-16 18:22:33 +0200

    removed teleport when npc entere rooms

--- characters.py
+++ characters.py
@@ -80,7 +80,7 @@ class Character():
 									item = room.itemByCoordinates[localisedEntry]
 									break
 								else:
-									room.addCharacter(self,localisedEntry[0],localisedEntry[1])
+									room.addCharacter(self,localisedEntry[0],localisedEntry[1]+1)
 									self.terrain.characters.remove(self)
 									self.terrain = None
 									self.changed()
@@ -96,7 +96,7 @@ class Character():
 									item = room.itemByCoordinates[localisedEntry]
 									break
 								else:
-									room.addCharacter(self,localisedEntry[0],localisedEntry[1])
+									room.addCharacter(self,localisedEntry[0],localisedEntry[1]-1)
 									self.terrain.characters.remove(self)
 									self.terrain = None
 									self.changed()

b58c50a381c2837e784c2026bf09141cb4437f73
MarxMustermann <illumination666gmail.com>
2018-04-07 14:28:58 +0200

    added an event for showing messages

--- src/events.py
+++ src/events.py
@@ -10,6 +10,14 @@ class Event(object):
     def handleEvent(self):
         pass
 
+class ShowMessageEvent(Event):
+    def __init__(self,tick,message):
+        super().__init__(tick)
+        self.message = message
+
+    def handleEvent(self):
+        messages.append(self.message)
+
 class ShowCinematicEvent(Event):
     def __init__(self,tick,cinematic):
         super().__init__(tick)

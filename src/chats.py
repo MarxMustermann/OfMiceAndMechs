1e8b1362c1d7f5b19ae9fce742a377781378aaa9
MarxMustermann <illumination666gmail.com>
2018-09-09 19:06:33 +0200

    changed subSelf to self

--- src/chats.py
+++ src/chats.py
@@ -15,10 +15,10 @@ class FirstChat(interaction.SubMenu):
     '''
     straightforward state setting
     '''
-    def __init__(subSelf,partner):
-        subSelf.done = False
-        subSelf.persistentText = ""
-        subSelf.firstRun = True
+    def __init__(self,partner):
+        self.done = False
+        self.persistentText = ""
+        self.firstRun = True
         super().__init__()
 
     '''
@@ -32,18 +32,18 @@ class FirstChat(interaction.SubMenu):
     '''
     show the dialog for one keystroke
     '''
-    def handleKey(subSelf, key):
-        if subSelf.firstRun:
+    def handleKey(self, key):
+        if self.firstRun:
             # show fluffed up information
-            subSelf.persistentText = "indeed.\n\nI am "+subSelf.firstOfficer.name+" and do the acceptance tests. This means i order you to do some things and you will comply.\n\nYour implant will store the orders given. When you press q you will get a list of your current orders. Try to get familiar with the implant,\nit is an important tool for keeping things in order.\n\nDo not mind that the tests seem somewhat without purpose, protocol demands them and after you complete the test you will serve as an hooper on the Falkenbaum."
+            self.persistentText = "indeed.\n\nI am "+self.firstOfficer.name+" and do the acceptance tests. This means i order you to do some things and you will comply.\n\nYour implant will store the orders given. When you press q you will get a list of your current orders. Try to get familiar with the implant,\nit is an important tool for keeping things in order.\n\nDo not mind that the tests seem somewhat without purpose, protocol demands them and after you complete the test you will serve as an hooper on the Falkenbaum."
             messages.append("press q to see your questlist")
             interaction.submenue = None
-            subSelf.set_text(subSelf.persistentText)
+            self.set_text(self.persistentText)
 
             # remove chat option
             # bad code: this removal results in bugs if to chats of the same type exist
             # bad pattern: chat option stored as references to class complicates this
-            for item in subSelf.firstOfficer.basicChatOptions:
+            for item in self.firstOfficer.basicChatOptions:
                 if not isinstance(item,dict):
                     if item == FirstChat:
                         toRemove = item
@@ -52,14 +52,14 @@ class FirstChat(interaction.SubMenu):
                     if item["chat"] == FirstChat:
                         toRemove = item
                         break
-            subSelf.firstOfficer.basicChatOptions.remove(toRemove)
+            self.firstOfficer.basicChatOptions.remove(toRemove)
 
             # trigger further action
-            subSelf.phase.examineStuff()
+            self.phase.examineStuff()
             return False
         else:
             # finish
-            subSelf.done = True
+            self.done = True
             return True
 
 '''
@@ -74,13 +74,13 @@ class FurnaceChat(interaction.SubMenu):
     '''
     straightforward state setting
     '''
-    def __init__(subSelf,partner):
-        subSelf.state = None
-        subSelf.partner = partner
-        subSelf.firstRun = True
-        subSelf.done = False
-        subSelf.persistentText = ""
-        subSelf.submenue = None
+    def __init__(self,partner):
+        self.state = None
+        self.partner = partner
+        self.firstRun = True
+        self.done = False
+        self.persistentText = ""
+        self.submenue = None
         super().__init__()
 
     '''
@@ -95,16 +95,16 @@ class FurnaceChat(interaction.SubMenu):
     '''
     offer the player a option to go deeper
     '''
-    def handleKey(subSelf, key):
-        if subSelf.submenue:
-            if not subSelf.submenue.handleKey(key):
+    def handleKey(self, key):
+        if self.submenue:
+            if not self.submenue.handleKey(key):
                 # let the selection option hande the keystroke
                 return False
             else:
-                subSelf.done = True
+                self.done = True
 
                 # remove self from the chracters chat
-                for item in subSelf.firstOfficer.basicChatOptions:
+                for item in self.firstOfficer.basicChatOptions:
                     if not isinstance(item,dict):
                         if item == FurnaceChat:
                             toRemove = item
@@ -113,7 +113,7 @@ class FurnaceChat(interaction.SubMenu):
                         if item["chat"] == FirstChat:
                             toRemove = item
                             break
-                subSelf.firstOfficer.basicChatOptions.remove(toRemove)
+                self.firstOfficer.basicChatOptions.remove(toRemove)
              
                 # clear submenue
                 # bad code: direct state setting
@@ -121,29 +121,29 @@ class FurnaceChat(interaction.SubMenu):
                 interaction.loop.set_alarm_in(0.0, callShow_or_exit, '~')
 
                 # do the selected action
-                subSelf.submenue.selection()
+                self.submenue.selection()
                 return True
 
         # do first part of the dialog
-        if subSelf.firstRun:
+        if self.firstRun:
             # show information
-            subSelf.persistentText = ["There are some growth tanks (",displayChars.indexedMapping[displayChars.growthTank_filled],"/",displayChars.indexedMapping[displayChars.growthTank_unfilled],"), walls (",displayChars.indexedMapping[displayChars.wall],"), a pile of coal (",displayChars.indexedMapping[displayChars.pile],") and a furnace (",displayChars.indexedMapping[displayChars.furnace_inactive],"/",displayChars.indexedMapping[displayChars.furnace_active],")."]
-            subSelf.set_text(subSelf.persistentText)
+            self.persistentText = ["There are some growth tanks (",displayChars.indexedMapping[displayChars.growthTank_filled],"/",displayChars.indexedMapping[displayChars.growthTank_unfilled],"), walls (",displayChars.indexedMapping[displayChars.wall],"), a pile of coal (",displayChars.indexedMapping[displayChars.pile],") and a furnace (",displayChars.indexedMapping[displayChars.furnace_inactive],"/",displayChars.indexedMapping[displayChars.furnace_active],")."]
+            self.set_text(self.persistentText)
 
             # add new chat option
-            subSelf.firstOfficer.basicChatOptions.append(InfoChat)
+            self.firstOfficer.basicChatOptions.append(InfoChat)
                         
             # offer a selection of different story phasses
             options = {}
             niceOptions = {}
             counter = 1
-            for quest in subSelf.terrain.waitingRoom.quests:
+            for quest in self.terrain.waitingRoom.quests:
                 options[str(counter)] = quest
                 niceOptions[str(counter)] = quest.description.split("\n")[0]
                 counter += 1
-            options = {1:subSelf.phase.fireFurnaces,2:subSelf.phase.noFurnaceFirering}
+            options = {1:self.phase.fireFurnaces,2:self.phase.noFurnaceFirering}
             niceOptions = {1:"Yes",2:"No"}
-            subSelf.submenue = interaction.SelectionMenu("Say, do you like furnaces?",options,niceOptions)
+            self.submenue = interaction.SelectionMenu("Say, do you like furnaces?",options,niceOptions)
 
         return False
 
@@ -159,19 +159,19 @@ class SternChat(interaction.SubMenu):
     '''
     straight forwar state setting
     '''
-    def __init__(subSelf,partner):
-        subSelf.submenue = None
-        subSelf.firstRun = True
-        subSelf.done = False
+    def __init__(self,partner):
+        self.submenue = None
+        self.firstRun = True
+        self.done = False
         super().__init__()
 
     '''
     show the dialog for one keystroke
     '''
-    def handleKey(subSelf, key):
-        if subSelf.firstRun:
+    def handleKey(self, key):
+        if self.firstRun:
             # show fluffed up information
-            subSelf.persistentText = """Stern did not actually modify the implant. The modification was done elsewhere.
+            self.persistentText = """Stern did not actually modify the implant. The modification was done elsewhere.
 But that is concerning the artworks, thats nothing you need to know.
 
 You need to know however that Sterns modification enhanced the implants guidance, control and communication abilities.
@@ -181,14 +181,14 @@ You can do so by pressing """+commandChars.autoAdvance+"""
 It is of limited practability though. It is mainly useful for stupid manual labor and often does not 
 do things the most efficent way. It will even try to handle conversion, wich does not allways lead to optimal results"""
             messages.append("press "+commandChars.autoAdvance+" to let the implant take control ")
-            subSelf.set_text(subSelf.persistentText)
-            subSelf.firstRun = False
+            self.set_text(self.persistentText)
+            self.firstRun = False
             return False
         else:
             # remove chat option and finish
             # bad code: crashes
             firstOfficer.basicChatOptions.remove(SternChat)
-            subSelf.done = True
+            self.done = True
             return True
 
 '''
@@ -203,34 +203,34 @@ class InfoChat(interaction.SubMenu):
     '''
     straight forwar state setting
     '''
-    def __init__(subSelf,partner):
-        subSelf.submenue = None
-        subSelf.firstRun = True
-        subSelf.done = False
+    def __init__(self,partner):
+        self.submenue = None
+        self.firstRun = True
+        self.done = False
         super().__init__()
 
     '''
     show the dialog for one keystroke
     '''
-    def handleKey(subSelf, key):
-        if subSelf.firstRun:
+    def handleKey(self, key):
+        if self.firstRun:
             # show fluffed up information
-            subSelf.persistentText = """yes and a lot of it. I will give you two of these things on your way:\n
+            self.persistentText = """yes and a lot of it. I will give you two of these things on your way:\n
 1. You will need to pick up most of the Information along the way. Ask around and talk to people.
 Asking questions may hurt your reputation, since you will apear like new growth. 
 You are, so do not heasitate to learn the neccesary Information before you have a reputation to loose.\n
 2. Do not rely on the implant to guide you through dificult tasks. 
 Sterns modifications are doing a good job for repetitive tasks but are no replacement
 for a brain.\n\n"""
-            subSelf.set_text(subSelf.persistentText)
-            subSelf.firstRun = False
+            self.set_text(self.persistentText)
+            self.firstRun = False
             return False
         else:
             # remove chat option and finish
             # bad code: crashes
             firstOfficer.basicChatOptions.remove(InfoChat)
             firstOfficer.basicChatOptions.append(SternChat)
-            subSelf.done = True
+            self.done = True
             return True
 
 '''
@@ -245,21 +245,21 @@ class ReReport(interaction.SubMenu):
     '''
     state initialization
     '''
-    def __init__(subSelf,partner):
-        subSelf.persistentText = ""
-        subSelf.firstRun = True
+    def __init__(self,partner):
+        self.persistentText = ""
+        self.firstRun = True
         super().__init__()
 
     '''
     scold the player and start intro
     '''
-    def handleKey(subSelf, key):
-        if subSelf.firstRun:
+    def handleKey(self, key):
+        if self.firstRun:
             # show message
-            subSelf.persistentText = "It seems you did not report for duty imediatly. Try to not repeat that"
-            subSelf.set_text(subSelf.persistentText)
-            subSelf.done = True
-            subSelf.firstRun = False
+            self.persistentText = "It seems you did not report for duty imediatly. Try to not repeat that"
+            self.set_text(self.persistentText)
+            self.done = True
+            self.firstRun = False
 
             # punish player
             mainChar.reputation -= 1

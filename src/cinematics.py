5992f649a6633708c9d60580d5379694bb9229de
MarxMustermann <illumination666gmail.com>
2018-12-03 19:38:16 +0100

    moved urwid specific code to urwid specific file

--- src/cinematics.py
+++ src/cinematics.py
@@ -8,6 +8,8 @@
 import urwid
 import json
 
+import urwidSpecials
+
 # import basic internal libs
 import src.saveing
 
@@ -261,39 +263,8 @@ class TextCinematic(BasicCinematic):
         else:
            self.footerText = ""
 
-        """
-        split a mix of strings and urwid formating into a list where each element contains exactly
-        one character and its formating
-        bad code: this should not be an inline function but accessible as a helper
-        """
-        def flattenToPeseudoString(urwidText):
-
-            # split strings
-            if isinstance(urwidText,str):
-                return list(urwidText)
-
-            # recursively handle list items
-            elif isinstance(urwidText,list):
-                result = []
-                for item in urwidText:
-                    result.extend(flattenToPeseudoString(item))
-                return result
-
-            # resolve references
-            elif isinstance(urwidText,int):
-                result = flattenToPeseudoString(displayChars.indexedMapping[urwidText][1])
-                return result
-            
-            # handle formatters
-            else:
-                result = []
-                for item in flattenToPeseudoString(urwidText[1]):
-                    #result.append((urwidText[0],item)) # bad code: commented out code
-                    result.append(item) # bad code: nukes all the pretty colors
-                return result
-
         # flatten text and text information
-        self.text = flattenToPeseudoString(self.text)
+        self.text = urwidSpecials.flattenToPeseudoString(self.text)
         self.endPosition = len(self.text)
         if not scrolling:
             self.position = self.endPosition
@@ -312,20 +283,6 @@ class TextCinematic(BasicCinematic):
         if self.position > self.endPosition and not self.firstRun:
             return
         self.firstRun = False
-
-        '''
-        add rusty colors to a string
-        bad code: this should be a generally available function not an inline function
-        '''
-        def convert(payload):
-            converted = []
-            colours = ['#f50',"#a60","#f80","#fa0","#860"]
-            counter = 0
-            for char in payload:
-                counter += 1
-                if len(char):
-                    converted.append((urwid.AttrSpec(colours[counter*7%5],'default'),char))
-            return converted
 
         # scroll the text in char by char
         if self.position < self.endPosition:
@@ -348,7 +305,7 @@ class TextCinematic(BasicCinematic):
 
         # set or not set rusty colors
         if self.rusty:
-            base = convert(baseText)
+            base = urwidSpecials.makeRust(baseText)
         else:
             base = [baseText]
 

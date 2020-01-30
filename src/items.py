87435ad7f0e2aa8007342a28a7047e23f6efaafe
MarxMustermann <illumination666gmail.com>
2020-01-30 03:55:01 +0100

    added ability to share macros over gamestates

--- src/items.py
+++ src/items.py
@@ -2399,7 +2399,7 @@ class ProductionArtwork(Item):
             character.messages.append("cooldown not reached. Wait %s ticks"%(self.coolDown-(gamestate.tick-self.coolDownTimer),))
             return
 
-        excludeList = ("ProductionArtwork","Machine","Tree","Scrap","Corpse","Acid","Item","Pile","InfoScreen","CoalMine","BluePrint",)
+        excludeList = ("ProductionArtwork","Machine","Tree","Scrap","Corpse","Acid","Item","Pile","InfoScreen","CoalMine","BluePrint","GlobalMacroStorage")
 
         options = []
         for key,value in itemMap.items():
@@ -4678,6 +4678,118 @@ Also the number of ticks will be written to the register t.
 """
         return text
 
+'''
+'''
+class GlobalMacroStorage(Item):
+    type = "GlobalMacroStorage"
+
+    '''
+    call superclass constructor with modified parameters
+    '''
+    def __init__(self,xPosition=None,yPosition=None, name="global macro storage",creator=None):
+        super().__init__("mG",xPosition,yPosition,name=name,creator=creator)
+
+        self.initialState = self.getState()
+
+    def apply(self,character):
+
+        self.character = character
+
+        options = []
+        options.append(("load","load macro from global storage"))
+        options.append(("store","add macro to the global storage"))
+        self.submenue = interaction.SelectionMenu("what do you want to do",options)
+        self.character.macroState["submenue"] = self.submenue
+        self.character.macroState["submenue"].followUp = self.actionSelection
+
+    def actionSelection(self):
+        selection =  self.submenue.getSelection()
+        if selection == "load":
+            try:
+                with open("globalStorage.json","r") as globalStorageFile:
+                    globalStorage = json.loads(globalStorageFile.read())
+            except:
+                globalStorage = []
+
+            counter = 1
+
+            options = []
+            for entry in globalStorage:
+                options.append((counter,entry["name"]))
+                counter += 1
+            self.submenue = interaction.SelectionMenu("what macro do you want to load?",options)
+            self.character.macroState["submenue"] = self.submenue
+            self.character.macroState["submenue"].followUp = self.load
+
+        if selection == "store":
+            self.submenue = interaction.InputMenu("supply a name/description for the macro")
+            self.character.macroState["submenue"] = self.submenue
+            self.character.macroState["submenue"].followUp = self.store
+
+    def load(self):
+        try:
+            with open("globalStorage.json","r") as globalStorageFile:
+                globalStorage = json.loads(globalStorageFile.read())
+        except:
+            globalStorage = []
+
+        rawMacros = globalStorage[self.submenue.getSelection()-1]["macro"]
+        parsedMacros = {}
+
+        state = "normal"
+        for key,value in rawMacros.items():
+            parsedMacro = []
+            for char in value:
+                if state == "normal":
+                    if char == "/":
+                        state = "multi"
+                        combinedKey = ""
+                        continue
+                    parsedMacro.append(char)
+                if state == "multi":
+                    if char == "/":
+                        state = "normal"
+                        parsedMacro.append(combinedKey)
+                    else:
+                        combinedKey += char
+            parsedMacros[key] = parsedMacro
+
+        self.character.macroState["macros"] = parsedMacros
+        self.character.messages.append("you load the macro %s from the macro storage"%(globalStorage[self.submenue.getSelection()-1]["name"]))
+
+    def store(self):
+        try:
+            with open("globalStorage.json","r") as globalStorageFile:
+                globalStorage = json.loads(globalStorageFile.read())
+        except:
+            globalStorage = []
+
+        compressedMacros = {}
+        for key,value in self.character.macroState["macros"].items():
+            compressedMacro = ""
+            for keystroke in value:
+                if len(keystroke) == 1:
+                    compressedMacro += keystroke
+                else:
+                    compressedMacro += "/"+keystroke+"/"
+            compressedMacros[key] = compressedMacro
+
+        globalStorage.append({"name":self.submenue.text,"macro":compressedMacros})
+
+        with open("globalStorage.json","w") as globalStorageFile:
+            globalStorageFile.write(json.dumps(globalStorage, indent = 10, sort_keys = True))
+
+        self.character.messages.append("you store the macro in the global macro storage")
+
+    def getLongInfo(self):
+        text = """
+
+This device is a one of a kind machine. It allows to store and load marcos between gamestates.
+
+"""
+        return text
+
+
 # maping from strings to all items
 # should be extendable
 itemMap = {
@@ -4753,6 +4865,7 @@ itemMap = {
             "BackTracker":BackTracker,
             "Tumbler":Tumbler,
             "ItemUpgrader":ItemUpgrader,
+            "GlobalMacroStorage":GlobalMacroStorage,
 }
 
 producables = {

eae6a3fb587b53111b9e92089140c093e0f73753
UmbralReaper <67179462+UmbralReaper@users.noreply.github.com>
2020-07-28 08:25:25 +1000

    show example of pep 8
    Please also include the.gitignore. They are essential to keep my code editor functioning properly without weird shenanigans.'
    
    This is an example of proper PEP 8, excluding naming conventions.

--- executeYou.py
+++ executeYou.py
@@ -2,12 +2,12 @@
 
 import socket
 
-HOST = input("enter server adress")  # The server's hostname or IP address
-PORT = 65440        # The port used by the server
-
 import time
 import json
 import urwid
+
+HOST = input("enter server adress")  # The server's hostname or IP address
+PORT = 65440  # The port used by the server
 
 header = urwid.Text(u"")
 main = urwid.Text(u"")
@@ -15,14 +15,15 @@ footer = urwid.Text(u"")
 
 main.set_layout('left', 'clip')
 
-frame = urwid.Frame(urwid.Filler(main,"top"),header=header,footer=footer)
+frame = urwid.Frame(urwid.Filler(main, "top"), header=header, footer=footer)
+
 
 def draw(raw):
     def deserializeUrwid(inData):
         outData = []
         for item in inData:
             if item[0] == "tuple":
-                outData.append((urwid.AttrSpec(item[1][0],item[1][1]),deserializeUrwid(item[2])))
+                outData.append((urwid.AttrSpec(item[1][0], item[1][1]), deserializeUrwid(item[2])))
             if item[0] == "list":
                 outData.append(deserializeUrwid(item[1]))
             if item[0] == "str":
@@ -30,9 +31,10 @@ def draw(raw):
 
         return outData
 
-    #header.set_text((urwid.AttrSpec("default","default"),deserializeUrwid(raw["head"])))
-    main.set_text((urwid.AttrSpec("default","default"),deserializeUrwid(raw["main"])))
-    footer.set_text((urwid.AttrSpec("default","default"),deserializeUrwid(raw["footer"])))
+    # header.set_text((urwid.AttrSpec("default","default"),deserializeUrwid(raw["head"])))
+    main.set_text((urwid.AttrSpec("default", "default"), deserializeUrwid(raw["main"])))
+    footer.set_text((urwid.AttrSpec("default", "default"), deserializeUrwid(raw["footer"])))
+
 
 def getData(request=b'ignore'):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -46,17 +48,22 @@ def getData(request=b'ignore'):
                 break
         return data.decode("utf-8")
 
+
 commands = []
+
+
 def keyboardListener(key):
     header.set_text(str(key))
-    if not isinstance(key,str):
+    if not isinstance(key, str):
         return
 
     commands.append(key)
 
+
 loop = urwid.MainLoop(frame, unhandled_input=keyboardListener)
 
-def tmp3(loop,user_data):
+
+def tmp3(loop, user_data):
     if not commands:
         data = getData(request=b'redraw')
     else:
@@ -68,7 +75,7 @@ def tmp3(loop,user_data):
 
     loop.set_alarm_in(0.01, tmp3)
 
+
 loop.set_alarm_in(0.1, tmp3)
 
 loop.run()
-

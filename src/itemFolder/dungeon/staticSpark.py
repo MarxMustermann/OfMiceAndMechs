21d455e7ec57f97feee7d32edfb0c66dc30f628a
Your Name <you@example.com>
2021-05-16 15:38:24 +0200

    docstringed + light refactoring

--- src/itemFolder/dungeon/staticSpark.py
+++ src/itemFolder/dungeon/staticSpark.py
@@ -2,9 +2,17 @@ import src
 
 
 class StaticSpark(src.items.Item):
+    """
+    ingame item acting as a key for some locks
+    """
+
     type = "StaticSpark"
 
     def __init__(self):
+        """
+        configure super class
+        """
+
         super().__init__(display=src.canvas.displayChars.staticSpark)
 
         self.name = "static spark"
@@ -13,13 +21,16 @@ class StaticSpark(src.items.Item):
         self.bolted = False
         self.strength = 1
 
+    # abstraction: should use the same abstractacted logic as all other food
     def apply(self, character):
-        character.addSatiation(self.strength * 50)
-        character.addMessage(
-            "you gained "
-            + str(self.strength * 50)
-            + " satiation from consuming the spark"
-        )
+        """
+        handle a character trying to eat the spark
+
+        Parameters:
+            character: the character trying to eat the spark
+        """
+
+        character.addSatiation(self.strength * 50, reason="consuming a static spark")
         self.destroy(generateSrcap=False)
 
 

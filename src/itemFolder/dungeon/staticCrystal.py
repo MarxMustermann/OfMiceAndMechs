931cbed5f66db4cece513a7b24ceb4592352c72c
Your Name <you@example.com>
2021-05-16 15:24:16 +0200

    docstringed and removed old code

--- src/itemFolder/dungeon/staticCrystal.py
+++ src/itemFolder/dungeon/staticCrystal.py
@@ -2,18 +2,22 @@ import src
 
 
 class StaticCrystal(src.items.Item):
+    """
+    ingame item representing the "amulet of yendor" of my game
+    the player is expected tp retrieve this item to exchange it for a reward
+    """
+
     type = "StaticCrystal"
 
     def __init__(self):
+        """
+        configure the super class
+        """
+
         super().__init__(display=src.canvas.displayChars.staticCrystal)
         self.name = "static spark"
 
         self.walkable = False
         self.bolted = False
-
-    def apply(self, character):
-        self.character.addMessage("you break reality")
-        1 / 0
-
 
 src.items.addType(StaticCrystal)

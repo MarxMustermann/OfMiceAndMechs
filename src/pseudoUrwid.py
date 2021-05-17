fcbc20953464bd3b5dd52adc8ee750cc983ff285
Your Name <you@example.com>
2021-05-17 02:40:45 +0200

    more docstrings

--- src/pseudoUrwid.py
+++ src/pseudoUrwid.py
@@ -1,4 +1,17 @@
 class AttrSpec(object):
+    """
+    dumy object behaving like an urwid attribute
+    this is used to prevent crashes
+    """
+
     def __init__(self, fg, bg):
+        """
+        store basic attriutes
+
+        Parameters:
+            fg: foreground color
+            bg: background color
+        """
+
         self.fg = fg
         self.bg = bg

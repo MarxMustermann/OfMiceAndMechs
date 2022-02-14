200264f80fd4cba7fb263ff619f7ab1bf2647441
Your Name <you@example.com>
2022-02-15 00:41:37 +0100

    updated test file

--- soundTest.py
+++ soundTest.py
@@ -4,7 +4,7 @@ import soundfile as sf
 sound_clip, samplerate = sf.read('sounds/electroRoom.ogg',dtype='float32')
 
 device = tcod.sdl.audio.open()
-device.queue_audio(sound_clip)  # Play audio synchronously.
+#device.queue_audio(sound_clip)  # Play audio synchronously.
 
 mixer = tcod.sdl.audio.BasicMixer(device)
 mixer.play(sound_clip)  # Play audio asynchronously.

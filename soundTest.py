a9e1904f0b14cca3234a8b66af3702effce0754e
Your Name <you@example.com>
2022-02-16 13:27:10 +0100

    new sound test

--- soundTest.py
+++ soundTest.py
@@ -7,7 +7,9 @@ device = tcod.sdl.audio.open()
 #device.queue_audio(sound_clip)  # Play audio synchronously.
 
 mixer = tcod.sdl.audio.BasicMixer(device)
-mixer.play(sound_clip)  # Play audio asynchronously.
+mixer.start()
+channel = mixer.get_channel("test")
+channel.play(sound_clip)
 
 import time
 time.sleep(60)

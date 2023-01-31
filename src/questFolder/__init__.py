4a508c682d48f88a670baeb042e7d0fde18af078
Your Name <you@example.com>
2023-01-31 01:58:01 +0100

    moved quest to seperate file

--- src/questFolder/__init__.py
+++ src/questFolder/__init__.py
@@ -1,3 +1,4 @@
 import src.questFolder.killPatrolers
+import src.questFolder.killGuards
 import src.questFolder.beUsefull
 import src.questFolder.beUsefullOnTile
--- src/quests.py
+++ src/quests.py
@@ -9545,93 +9545,6 @@ The Hives are shown on the minimap as: """,(src.interaction.urwid.AttrSpec("#484
             self.addQuest(quest)
         super().solver(character)
 
-class KillGuards(MetaQuestSequence):
-    def __init__(self, description="kill guards"):
-        super().__init__()
-        self.metaDescription = description
-        self.type = "KillGuards"
-
-    def generateTextDescription(self):
-        return """
-Eliminate the siege guard.
-
-There is a group of enemies near entry of the base.
-They guard the entrance of the base against outbreak or resupplies.
-The guards are shown as white <-
-
-Eliminate them to start breaking up the innermost siege ring.
-Remove guards from the tiles (7,4,0) and (6,5,0)"""
-
-    def postHandler(self):
-        if self.character == src.gamestate.gamestate.mainChar:
-            src.gamestate.gamestate.save()
-        super().postHandler()
-
-    def solver(self,character):
-        if self.triggerCompletionCheck(character):
-            return
-
-        if not self.subQuests:
-            guard = random.choice(self.getGuards(character))
-            quest = SecureTile(toSecure=guard.getBigPosition(),endWhenCleared=True,description="kill guards on tile ")
-            self.addQuest(quest)
-            quest.assignToCharacter(character)
-            quest.activate()
-            return
-        super().solver(character)
-
-    def wrapedTriggerCompletionCheck2(self, extraInfo):
-        if not self.active:
-            return
-
-        self.triggerCompletionCheck(extraInfo["character"])
-
-    def assignToCharacter(self, character):
-        if self.character:
-            return
-
-        self.startWatching(character,self.wrapedTriggerCompletionCheck2, "character died on tile")
-
-        super().assignToCharacter(character)
-
-    def triggerCompletionCheck(self,character=None):
-        if not character:
-            return False
-
-        if not self.getGuards(character):
-            character.awardReputation(amount=250, reason="killing the guards")
-            self.postHandler()
-            return True
-
-        return False
-
-    def getGuards(self,character):
-        enemies = []
-        currentTerrain = character.getTerrain()
-
-        foundEnemy = False
-        for enemy in currentTerrain.characters:
-            if enemy.tag == "blocker":
-                enemies.append(enemy)
-        for room in currentTerrain.rooms:
-            for enemy in room.characters:
-                if enemy.tag == "blocker":
-                    enemies.append(enemy)
-
-        return enemies
-
-    def getQuestMarkersTile(self,character):
-        result = super().getQuestMarkersTile(character)
-        positions = []
-        for guard in self.getGuards(character):
-            pos = guard.getBigPosition()
-            if not pos in positions:
-                positions.append(pos)
-
-        for pos in positions:
-            result.append((pos,"target"))
-        return result
-
 class Heal(MetaQuestSequence):
     def __init__(self, description="heal"):
         super().__init__()
@@ -11069,7 +10982,6 @@ questMap = {
     "CleanTraps": CleanTraps,
     "ReloadTraps": ReloadTraps,
     "ClearInventory": ClearInventory,
-    "KillGuards": KillGuards,
     "DestroySpawners": DestroySpawners,
     "TakeOverBase": TakeOverBase,
     "GetQuestFromQuestArtwork": GetQuestFromQuestArtwork,

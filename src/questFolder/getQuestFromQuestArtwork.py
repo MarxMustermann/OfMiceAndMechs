58a58c8f4d362f42c4def1d9a6e9829929712b9b
Your Name <you@example.com>
2023-01-31 03:00:42 +0100

    brought solver to current logic

--- src/questFolder/getQuestFromQuestArtwork.py
+++ src/questFolder/getQuestFromQuestArtwork.py
@@ -38,17 +38,30 @@ Activate the quest artwork to fetch a quest."""
 
         super().assignToCharacter(character)
 
-    def solver(self,character):
+    def solver(self, character):
         if not self.subQuests:
-            room = character.container
-
-            if not isinstance(character.container, src.rooms.Room):
-                quest = src.quests.questMap["GoHome"]()
-                quest.active = True
-                quest.assignToCharacter(character)
-                self.addQuest(quest)
+            self.generateSubquests(character)
+            if self.subQuests:
                 return
 
+            command = self.getSolvingCommandString(character)
+            if command:
+                character.runCommandString(command)
+                return
+
+        super().solver(character)
+
+    def getSolvingCommandString(self,character):
+        if self.subQuests:
+            return
+
+        submenue = character.macroState.get("submenue")
+        if submenue:
+            if isinstance(submenue,src.interaction.SelectionMenu):
+                return ["enter"]
+            return ["esc"]
+
+        if isinstance(character.container, src.rooms.Room):
             for item in room.itemsOnFloor:
                 if not item.bolted:
                     continue
@@ -64,30 +77,46 @@ Activate the quest artwork to fetch a quest."""
                         direction = "s"
                     
                     if direction:
-                        quest = src.quests.questMap["RunCommand"](command=list("J"+direction+".j")+3*["enter"],description="activate quest artwork ")
-                        self.addQuest(quest)
-                        quest.assignToCharacter(character)
-                        quest.activate()
-                        return 
+                        return list("J"+direction+".j")+3*["enter"]
+        return super().getSolvingCommandString(character)
 
-                    quest = src.quests.questMap["GoToPosition"](targetPosition=item.getPosition(),ignoreEndBlocked=True,description="go to quest artwork ")
-                    quest.active = True
-                    quest.assignToCharacter(character)
-                    self.addQuest(quest)
-                    return
+    def generateSubquests(self,character):
+        if self.subQuests:
+            return
 
-            if not "Questing" in character.duties:
-                directions = [(-1,0),(1,0),(0,-1),(0,1)]
-                random.shuffle(directions)
-                for direction in directions:
-                    newPos = (room.xPosition+direction[0],room.yPosition+direction[1])
-                    if room.container.getRoomByPosition(newPos):
-                        self.addQuest(src.quests.questMap["GoToTile"](targetPosition=newPos))
-                        return
+        room = character.container
+
+        if not isinstance(character.container, src.rooms.Room):
+            quest = src.quests.questMap["GoHome"]()
+            quest.active = True
+            quest.assignToCharacter(character)
+            self.addQuest(quest)
+            return
+
+        for item in room.itemsOnFloor:
+            if not item.bolted:
+                continue
+            if item.type == "QuestArtwork":
+                direction = None
+                if item.getPosition() == (character.xPosition-1,character.yPosition,0):
+                    direction = "a"
+                if item.getPosition() == (character.xPosition+1,character.yPosition,0):
+                    direction = "d"
+                if item.getPosition() == (character.xPosition,character.yPosition-1,0):
+                    direction = "w"
+                if item.getPosition() == (character.xPosition,character.yPosition+1,0):
+                    direction = "s"
+                
+                if direction:
+                    return 
+
+                quest = src.quests.questMap["GoToPosition"](targetPosition=item.getPosition(),ignoreEndBlocked=True,description="go to quest artwork ")
+                quest.active = True
+                quest.assignToCharacter(character)
+                self.addQuest(quest)
                 return
-            else:
-                self.addQuest(src.quests.questMap["GoToTile"](targetPosition=(7,7,0),description="go to command centre"))
-                return
-        super().solver(character)
+
+        self.addQuest(src.quests.questMap["GoToTile"](targetPosition=(7,7,0),description="go to command centre"))
+        return
 
 src.quests.addType(GetQuestFromQuestArtwork)

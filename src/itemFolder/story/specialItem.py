a5ee0da40aac103723448f56b222173194b0fea6
Marx Mustermann <marxmustermann@riseup.net>
2024-04-13 00:47:46 +0200

    color coded glass hearts

--- src/itemFolder/story/specialItem.py
+++ src/itemFolder/story/specialItem.py
@@ -24,5 +24,27 @@ class SpecialItem(src.items.Item):
     def getLongInfo(self):
         return f"{self.itemID}"
 
+    def render(self):
+        color = "#888"
+        if self.itemID == 1:
+            color = "#f00"
+        elif self.itemID == 2:
+            color = "#0f0"
+        elif self.itemID == 3:
+            color = "#00f"
+        elif self.itemID == 4:
+            color = "#0ff"
+        elif self.itemID == 5:
+            color = "#f0f"
+        elif self.itemID == 6:
+            color = "#ff0"
+        elif self.itemID == 7:
+            color = "#fff"
+        displaychars = "gh"
+
+        display = [
+                (src.interaction.urwid.AttrSpec(color, "black"), displaychars),
+            ]
+        return display
 
 src.items.addType(SpecialItem)

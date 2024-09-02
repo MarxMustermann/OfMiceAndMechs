8379628ea9f8d0045f9ce3794700f372520e6b21
MarxMustermann <marxmustermann@riseup.net>
2024-09-02 23:23:41 +0200

    made wall unboltable

--- src/itemFolder/resources/wall.py
+++ src/itemFolder/resources/wall.py
@@ -16,4 +16,29 @@ class Wall(src.items.Item):
         '''
         super().__init__(display=src.canvas.displayChars.wall)
 
+    def getConfigurationOptions(self, character):
+        """
+        register the configuration options with superclass
+
+        Parameters:
+            character: the character trying to conigure the machine
+        """
+
+        options = super().getConfigurationOptions(character)
+        if self.bolted:
+            options["b"] = ("unbolt", self.unboltAction)
+        else:
+            options["b"] = ("bolt down", self.boltAction)
+        return options
+
+    def boltAction(self,character):
+        self.bolted = True
+        character.addMessage("you bolt down the Wall")
+        character.changed("boltedItem",{"character":character,"item":self})
+
+    def unboltAction(self,character):
+        self.bolted = False
+        character.addMessage("you unbolt the Wall")
+        character.changed("unboltedItem",{"character":character,"item":self})
+
 src.items.addType(Wall)

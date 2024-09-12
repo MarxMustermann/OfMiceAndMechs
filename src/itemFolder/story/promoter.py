e43a5a92c79b878b808fe42044c89a839295a1f2
MarxMustermann <marxmustermann@riseup.net>
2024-09-13 00:04:18 +0200

    added very minimal promotion functionality

---
+++ src/itemFolder/story/promoter.py
@@ -0,0 +1,26 @@
+import src
+import random
+
+class Promoter(src.items.Item):
+    """
+    """
+
+    type = "Promoter"
+
+    def __init__(self,):
+        """
+        configure the superclass
+        """
+
+        super().__init__(display="PR")
+        self.faction = None
+
+        self.walkable = False
+        self.bolted = True
+
+    def apply(self,character):
+        if character.rank > 2:
+            character.rank = character.rank-1
+        character.addMessage(f"you were promoted to rank {character.rank}")
+
+src.items.addType(Promoter)

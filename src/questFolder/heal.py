c4adc543e45cc2a8c487e8509877e097f00f8dd0
MarxMustermann <marxmustermann@riseup.net>
2024-09-22 17:21:38 +0200

    added healing from using a coal burner

--- src/questFolder/heal.py
+++ src/questFolder/heal.py
@@ -1,5 +1,5 @@
 import src
-
+import random
 
 class Heal(src.quests.MetaQuestSequence):
     type = "Heal"
@@ -14,23 +14,75 @@ You are hurt. Heal yourself.
 
 You can heal yourself using vials.
 Use vials to heal yourself.
-
 Press JH to auto heal.
+
 """
         return text
 
-    def triggerCompletionCheck(self,character=None):
-        if not character:
-            return False
+    def getSolvingCommandString(self, character, dryRun=True):
+        nextStep = self.getNextStep(character)
+        if nextStep == (None,None):
+            return super().getSolvingCommandString(character)
+        return self.getNextStep(character)[1]
 
+    def getNextStep(self,character=None,ignoreCommands=False,dryRun=True):
+
+        if self.subQuests:
+            return (None,None)
+
+        if not character:
+            return (None,None)
+
+        # heal using vials
         foundVial = None
         for item in character.inventory:
             if item.type == "Vial" and item.uses > 0:
                 foundVial = item
+        if foundVial:
+            return (None,("JH","drink from vial"))
 
-        if not foundVial:
-            self.postHandler()
-            return True
+        # heal using coal burners
+        terrain = character.getTerrain()
+        rooms = terrain.rooms[:]
+        if character.container.isRoom:
+            rooms.insert(0,character.container)
+        for room in rooms:
+            items = room.getItemsByType("CoalBurner",needsBolted=True)
+            foundBurners = []
+            for item in items:
+                if not item.getMoldFeed(character):
+                    continue
+                foundBurners.append(item)
+
+            if not foundBurners:
+                continue
+
+            if not character.container == room:
+                quest = src.quests.questMap["GoToTile"](targetPosition=room.getPosition())
+                return ([quest],None)
+
+            for item in foundBurners:
+                direction = None
+                if character.getPosition(offset=(1,0,0)) == item.getPosition():
+                    direction = "d"
+                if character.getPosition(offset=(-1,0,0)) == item.getPosition():
+                    direction = "a"
+                if character.getPosition(offset=(0,1,0)) == item.getPosition():
+                    direction = "s"
+                if character.getPosition(offset=(0,-1,0)) == item.getPosition():
+                    direction = "w"
+
+                if direction:
+                    return (None,("J"+direction,"inhale smoke"))
+
+            quest = src.quests.questMap["GoToPosition"](targetPosition=random.choice(foundBurners).getPosition(),ignoreEndBlocked=True)
+            return ([quest],None)
+
+        return (None,None)
+
+    def triggerCompletionCheck(self,character=None):
+        if not character:
+            return False
 
         if character.health < character.maxHealth:
             return False
@@ -38,18 +90,19 @@ Press JH to auto heal.
         self.postHandler()
         return True
 
-    def getSolvingCommandString(self,character,dryRun=True):
-        if not dryRun:
-            self.triggerCompletionCheck(character)
-        return "JH"
-
-    def solver(self,character):
-        command = self.getSolvingCommandString(character,dryRun=False)
-        if command:
-            character.runCommandString(command)
-            return None
+    def solver(self, character):
         if self.triggerCompletionCheck(character):
-            return None
-        return super().solver(character)
+            return
+
+        (nextQuests,nextCommand) = self.getNextStep(character)
+        if nextQuests:
+            for quest in nextQuests:
+                self.addQuest(quest)
+            return
+
+        if nextCommand:
+            character.runCommandString(nextCommand[0])
+            return
+        super().solver(character)
 
 src.quests.addType(Heal)

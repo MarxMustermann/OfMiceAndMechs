55295634db7b25d05e1624177c73c923b43f3804
MarxMustermann <marxmustermann@riseup.net>
2024-09-22 23:07:46 +0200

    removed lie

--- src/questFolder/story/goToTileStory.py
+++ src/questFolder/story/goToTileStory.py
@@ -18,8 +18,6 @@ class GoToTileStory(src.quests.questMap["GoToTile"]):
         return [f"""
 Go to the base entrance located on tile {self.targetPosition}{reasonString}.
 Avoid fighting with the insects, you are not equipped for it.
-
-The insects are shown as: """,(src.interaction.urwid.AttrSpec("#fff", "#f00"), "<-"),"""
 Enemies are shown with a red background.
 
 This quest is part of the quest to secure the base.

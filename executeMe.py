297439d6ebc3f6f0f4e918f1e0da270362e13673
hero2002 <abdkafi2002@gmail.com>
2024-10-07 21:02:10 +0300

    Revert "disabled sound"
    This reverts commit 80290e823031f825c1acae6d5ae357e8f7af903c.

--- executeMe.py
+++ executeMe.py
@@ -187,6 +187,6 @@ if __name__ == '__main__':
                         raise SystemExit()
             """
     except Exception as e:
-        #src.interaction.tcodMixer.stop()
+        src.interaction.tcodMixer.stop()
 
         raise e

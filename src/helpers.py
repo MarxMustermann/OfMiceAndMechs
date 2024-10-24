fecc59c14092eccbb03372315d4e1175ec42ae06
MarxMustermann <marxmustermann@riseup.net>
2024-10-24 22:10:56 +0200

    removed deprecation warning

--- src/helpers.py
+++ src/helpers.py
@@ -15,29 +15,29 @@ def clamp(n, min, max):
 def draw_frame_text(width, height, t, x, y):
     for cx in range(x-1,x+width):
         for cy in range(y-1,y+height):
-            src.interaction.tcodConsole.rgb[cx,cy] = ord(" "), tcod.black, tcod.black
+            src.interaction.tcodConsole.rgb[cx,cy] = ord(" "), (0, 0, 0),(0, 0, 0) 
     for cx in range(x-1,x+width):
-        src.interaction.tcodConsole.rgb[cx,y-1] = ord("-"), tcod.white, tcod.black
-        src.interaction.tcodConsole.rgb[cx,y+height] = ord("-"), tcod.white, tcod.black
+        src.interaction.tcodConsole.rgb[cx,y-1] = ord("-"), (255, 255, 255),(0, 0, 0) 
+        src.interaction.tcodConsole.rgb[cx,y+height] = ord("-"), (255, 255, 255),(0, 0, 0) 
     for cy in range(y-1,y+height):
-        src.interaction.tcodConsole.rgb[x-1,cy] = ord("|"), tcod.white, tcod.black
-        src.interaction.tcodConsole.rgb[x+width,cy] = ord("|"), tcod.white, tcod.black
+        src.interaction.tcodConsole.rgb[x-1,cy] = ord("|"), (255, 255, 255),(0, 0, 0) 
+        src.interaction.tcodConsole.rgb[x+width,cy] = ord("|"), (255, 255, 255),(0, 0, 0) 
 
-    src.interaction.tcodConsole.rgb[x-1,y-1] = ord("+"), tcod.white, tcod.black
-    src.interaction.tcodConsole.rgb[x-1,y-2] = ord("|"), tcod.white, tcod.black
-    src.interaction.tcodConsole.rgb[x-2,y-1] = ord("-"), tcod.white, tcod.black
+    src.interaction.tcodConsole.rgb[x-1,y-1] = ord("+"), (255, 255, 255),(0, 0, 0) 
+    src.interaction.tcodConsole.rgb[x-1,y-2] = ord("|"), (255, 255, 255),(0, 0, 0) 
+    src.interaction.tcodConsole.rgb[x-2,y-1] = ord("-"), (255, 255, 255),(0, 0, 0) 
 
-    src.interaction.tcodConsole.rgb[x+width,y-1] = ord("+"), tcod.white, tcod.black
-    src.interaction.tcodConsole.rgb[x+width,y-2] = ord("|"), tcod.white, tcod.black
-    src.interaction.tcodConsole.rgb[x+width+1,y-1] = ord("-"), tcod.white, tcod.black
+    src.interaction.tcodConsole.rgb[x+width,y-1] = ord("+"), (255, 255, 255),(0, 0, 0) 
+    src.interaction.tcodConsole.rgb[x+width,y-2] = ord("|"), (255, 255, 255),(0, 0, 0) 
+    src.interaction.tcodConsole.rgb[x+width+1,y-1] = ord("-"), (255, 255, 255),(0, 0, 0) 
     
-    src.interaction.tcodConsole.rgb[x-1,y+height] = ord("+"), tcod.white, tcod.black
-    src.interaction.tcodConsole.rgb[x-1,y+height+1] = ord("|"), tcod.white, tcod.black
-    src.interaction.tcodConsole.rgb[x-2,y+height] = ord("-"), tcod.white, tcod.black
+    src.interaction.tcodConsole.rgb[x-1,y+height] = ord("+"), (255, 255, 255),(0, 0, 0) 
+    src.interaction.tcodConsole.rgb[x-1,y+height+1] = ord("|"), (255, 255, 255),(0, 0, 0) 
+    src.interaction.tcodConsole.rgb[x-2,y+height] = ord("-"), (255, 255, 255),(0, 0, 0) 
 
-    src.interaction.tcodConsole.rgb[x+width,y+height] = ord("+"), tcod.white, tcod.black
-    src.interaction.tcodConsole.rgb[x+width,y+height+1] = ord("|"), tcod.white, tcod.black
-    src.interaction.tcodConsole.rgb[x+width+1,y+height] = ord("-"), tcod.white, tcod.black
+    src.interaction.tcodConsole.rgb[x+width,y+height] = ord("+"), (255, 255, 255),(0, 0, 0) 
+    src.interaction.tcodConsole.rgb[x+width,y+height+1] = ord("|"), (255, 255, 255),(0, 0, 0) 
+    src.interaction.tcodConsole.rgb[x+width+1,y+height] = ord("-"), (255, 255, 255),(0, 0, 0) 
 
 
     src.interaction.tcodConsole.print_box(

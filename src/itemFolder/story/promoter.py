ee4408cfa415c7b94d07cb963b5e6e2d8175766e
MarxMustermann <marxmustermann@riseup.net>
2024-10-27 16:00:44 +0100

    made promoter promote in steps

--- src/itemFolder/story/promoter.py
+++ src/itemFolder/story/promoter.py
@@ -19,7 +19,22 @@ class Promoter(src.items.Item):
         self.bolted = True
 
     def apply(self,character):
-        if character.rank > 2:
+        if character.rank > 5:
+            character.rank = 5
+            character.hasSpecialAttacks = True
+            
+            character.addMessage(f"you were promoted to rank 5")
+            submenu = src.menuFolder.TextMenu.TextMenu("""
+You put your head into the machine.
+
+Its tendrils reach out and touch your implant.
+
+It is upgraded to rank 5.
+This means you can do special attacks now.""")
+            character.macroState["submenue"] = submenu
+            character.runCommandString("~",nativeKey=True)
+
+        elif character.rank > 2:
             character.rank = 2
             character.addMessage(f"you were promoted to base commander")
             submenu = src.menuFolder.TextMenu.TextMenu("""
@@ -28,10 +43,9 @@ You put your head into the machine.
 Its tendrils reach out and touch your implant.
 
 It is upgraded to rank 2.
-This means you can do special attacks now.""")
+""")
             character.macroState["submenue"] = submenu
             character.runCommandString("~",nativeKey=True)
-            character.hasSpecialAttacks = True
         character.changed("got promotion",{})
 
 src.items.addType(Promoter)

b46b36652ef383c9277e461c452f0ac8af21c250
MarxMustermann <marxmustermann@riseup.net>
2024-12-09 17:03:26 +0100

    fixed typo

--- src/PopupFolder/EnterTrapRoom.py
+++ src/PopupFolder/EnterTrapRoom.py
@@ -7,12 +7,12 @@ class EnterTrapRoom(src.popups.Popup):
         return "entered room"
 
     def text(self):
-        return """Your implant iterrupts:
+        return """Your implant interrupts:
 
 You just entered a trap room.
 Be careful and don't step onto the triggerPlates (_~)
 
-press q to get more detailed information.
+press esc to close this menu press q to get more detailed information.
 """
 
     def ConditionMet(self, params) -> bool:

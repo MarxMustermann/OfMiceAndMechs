e21b493e7e6fa2bfebc789c051dd92265bbdb7be
MarxMustermann <marxmustermann@riseup.net>
2024-12-13 18:28:41 +0100

    added reason and changed the slowdown to percentage and added description

--- src/statusEffectFolder/slowed.py
+++ src/statusEffectFolder/slowed.py
@@ -4,14 +4,26 @@ import src
 class Slowed(src.statusEffects.MovementBuff):
     type = "Slowed"
 
-    def __init__(self, slowDown=1.1, duration=100):
+    def __init__(self, slowDown=0.1, duration=100, reason=None):
         self.slowDown = slowDown
+        self.reason = reason
         super().__init__(duration)
 
     def ModMovement(self, speed):
-        return speed * self.slowDown
+        return speed * (1+self.slowDown)
 
     def getShortCode(self):
         return "SLW"
+
+    def getDescription(self):
+        text = ""
+        text += f"You were slowed and cannot move as fast any more"
+        text += f"\n"
+        if self.reason:
+            text += f"\nreason: {self.reason}"
+        text += f"\nslowDown: {self.slowDown}"
+        if self.ticks:
+            text += f"\nduration: {self.ticks}"
+        return text
 
 src.statusEffects.addType(Slowed)

7802d66ff454d17368a16a3c1c61431a3f89b9dc
MarxMustermann <marxmustermann@riseup.net>
2024-12-13 20:00:04 +0100

    added reason and moved to new description pattern

--- src/statusEffectFolder/berserk.py
+++ src/statusEffectFolder/berserk.py
@@ -4,10 +4,10 @@ import src
 class Berserk(src.statusEffects.DamageBuff,src.statusEffects.MovementBuff):
     type = "Berserk"
 
-    def __init__(self, damageBonus=2, duration=10, speedUp=0.1):
+    def __init__(self, damageBonus=2, duration=10, speedUp=0.1,reason=None):
         self.damageBonus = damageBonus
         self.speedUp = speedUp
-        super().__init__(duration)
+        super().__init__(duration=duration,reason=reason)
 
     def modDamage(self, attacker, attacked, bonus:str, damage):
         damage += self.damageBonus
@@ -27,10 +27,15 @@ class Berserk(src.statusEffects.DamageBuff,src.statusEffects.MovementBuff):
     def getShortCode(self):
         return "BSRK"
 
-    def getDescription(self):
+    def getLoreDescription(self):
         text = ""
         text += f"You are in a beserk rage and are ready to kill.\nYou chase your victims faster and hurt them harder.\n\nYou need to hurry and move fast or your rage runs out"
-        text += f"\n\nReason: You killed somebody\ndamage bonus: {self.damageBonus}\nduration: {self.ticks}"
         return text
+
+    def buildStatListDescription(self,description = ""):
+        description = super().buildStatListDescription(description=description)
+        description += f"damage bonus: {self.damageBonus}\n"
+        description += f"speed up: {self.speedUp*100}%\n"
+        return description
 
 src.statusEffects.addType(Berserk)

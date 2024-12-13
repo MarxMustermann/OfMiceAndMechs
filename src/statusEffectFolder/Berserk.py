079939cf50114913e6eec3ccf41bd635281c7184
MarxMustermann <marxmustermann@riseup.net>
2024-12-13 18:03:30 +0100

    fixed capitalisation

--- src/statusEffectFolder/Berserk.py
+++ src/statusEffectFolder/Berserk.py
@@ -4,12 +4,12 @@ import src
 class Berserk(src.statusEffects.DamageBuff,src.statusEffects.MovementBuff):
     type = "Berserk"
 
-    def __init__(self, DamageBonus=2, ticks=10):
-        self.DamageBonus = DamageBonus
+    def __init__(self, damageBonus=2, ticks=10):
+        self.damageBonus = damageBonus
         super().__init__(ticks)
 
     def ModDamage(self, attacker, attacked, bonus:str, damage):
-        damage += self.DamageBonus
+        damage += self.damageBonus
         if "and you gone Berserk" not in bonus:
             return (damage, bonus + "and you gone Berserk ")
 
@@ -24,7 +24,7 @@ class Berserk(src.statusEffects.DamageBuff,src.statusEffects.MovementBuff):
     def getDescription(self):
         text = ""
         text += f"You are in a beserk rage and are ready to kill.\nYou chase your victims faster and hurt them harder.\n\nYou need to hurry and move fast or your rage runs out"
-        text += f"\n\nReason: You killed somebody\nDamageBonus: {self.DamageBonus}\nduration: {self.ticks}"
+        text += f"\n\nReason: You killed somebody\ndamage bonus: {self.damageBonus}\nduration: {self.ticks}"
         return text
 
 src.statusEffects.addType(Berserk)

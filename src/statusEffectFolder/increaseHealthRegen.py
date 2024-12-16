9f2cb07900c3a15e1d187dde50e7a2aeb4adffa0
MarxMustermann <marxmustermann@riseup.net>
2024-12-16 17:46:36 +0100

    added reason parameter

--- src/statusEffectFolder/increaseHealthRegen.py
+++ src/statusEffectFolder/increaseHealthRegen.py
@@ -4,9 +4,9 @@ import src
 class IncreaseHealthRegen(src.statusEffects.HealthRegenBuff):
     type = "IncreaseHealthRegen"
 
-    def __init__(self, healthBonus=2, duration=500, inventoryItem=None):
+    def __init__(self, healthBonus=2, duration=500, inventoryItem=None, reason=None):
         self.healthBonus = healthBonus
-        super().__init__(duration=duration, inventoryItem=inventoryItem)
+        super().__init__(duration=duration, inventoryItem=inventoryItem, reason=reason)
 
     def modHealthRegen(self, health):
         return health + self.healthBonus

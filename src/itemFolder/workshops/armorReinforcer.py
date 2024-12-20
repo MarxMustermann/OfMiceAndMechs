466e7a5f4ecd10ffcc6dd11c01bd17fb9df6b5cf
MarxMustermann <marxmustermann@riseup.net>
2024-12-20 16:29:45 +0100

    made item multi use

--- src/itemFolder/workshops/armorReinforcer.py
+++ src/itemFolder/workshops/armorReinforcer.py
@@ -30,48 +30,58 @@ class ArmorReinforcer(src.items.itemMap["WorkShop"]):
             character.macroState["submenue"].followUp = {"container": self, "method": "ReinforceArmor", "params": params}
             return
 
-        citinPlates = None
+        chitinPlates = []
         for item in character.inventory:
-            if isinstance(item, src.items.itemMap["CitinPlates"]):
-                citinPlates = item
-                break
+            if not isinstance(item, src.items.itemMap["CitinPlates"]):
+                continue
+            chitinPlates.append(item)
 
-        if citinPlates is None:
-            character.addMessage("you don't have Citin Plates")
+        if not chitinPlates:
+            character.addMessage("you don't have Citin Plates, you only can improve your armor to 3")
             return
 
         armor = None
         if params["choice"] == "Reinforce Equipped Armor":
             if character.armor:
                 armor = character.armor
-                if armor.name == "improved Armor":
-                    character.addMessage("you can't upgrade the Armor twice")
-                    return
             else:
                 character.addMessage("you don't have any Armor equipped")
                 return
         else:
             for item in character.inventory:
-                if isinstance(item, src.items.itemMap["Armor"]) and armor.name != "improved Armor":
+                if isinstance(item, src.items.itemMap["Armor"]):
                     armor = item
                     break
             if armor is None:
                 character.addMessage("you don't have any base Armor in the inventory")
                 return
 
+        improvementAmount = 0
+        if armor.armorValue < 3:
+            improvementAmount += 3-armor.armorValue
+
+        for chitinPlate in chitinPlates:
+            if improvementAmount+armor.armorValue >= 8:
+                break
+
+            character.inventory.remove(chitinPlate)
+            improvementAmount += min(0.5,8-sword.armorValue-improvementAmount)
+
+        if not improvementAmount:
+            character.addMessage("you can't improve your armor")
+            return
+
         params["Armor"] = armor
-        params["productionTime"] = 100
+        params["productionTime"] = 100*improvementAmount
         params["doneProductionTime"] = 0
         params["hitCounter"] = character.numAttackedWithoutResponse
-
-        character.inventory.remove(CitinPlates)
+        params["improvementAmount"] = improvementAmount
 
         self.produceItem_wait(params)
 
     def produceItem_done(self, params):
         character = params["character"]
-        improvement_table = [(2, 5), (3, 3), (4, 2), (5, 1)]
-        improvement = random.choices([val[0] for val in improvement_table], [val[1] for val in improvement_table])[0]
+        improvement = params["improvementAmount"]
         character.addMessage(f"You improved the Armor by {improvement!s} points")
 
         armor = params["Armor"]

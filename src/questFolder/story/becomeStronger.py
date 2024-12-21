1641c3d4c48f30efb57e6647ecf9e5f000f82486
MarxMustermann <marxmustermann@riseup.net>
2024-12-21 02:36:37 +0100

    cleaned up

--- src/questFolder/story/becomeStronger.py
+++ src/questFolder/story/becomeStronger.py
@@ -52,37 +52,6 @@ class BecomeStronger(src.quests.MetaQuestSequence):
         quest = src.quests.questMap["Adventure"]()
         return ([quest],None)
 
-        return (None,None)
-
-        if not character.getBigPosition() == (7,8,0):
-            quest = src.quests.questMap["GoToTile"](targetPosition=(7,8,0),reason="go to spawning room",description="go to spawning room")
-            return ([quest],None)
-
-        if not character.container.isRoom:
-            return (None,None)
-
-        factionSetter = character.container.getItemByType("FactionSetter")
-        if not factionSetter:
-            self.fail(reason="no faction setter found")
-            return (None,None)
-
-        itemPos = factionSetter.getPosition()
-        if character.getDistance(itemPos) > 1:
-            quest = src.quests.questMap["GoToPosition"](targetPosition=factionSetter.getPosition(),reason="to be able to use the faction setter",description="go to faction setter",ignoreEndBlocked=True)
-            return ([quest],None)
-
-        direction = ""
-        if character.getPosition(offset=(1,0,0)) == itemPos:
-            direction = "d"
-        if character.getPosition(offset=(-1,0,0)) == itemPos:
-            direction = "a"
-        if character.getPosition(offset=(0,1,0)) == itemPos:
-            direction = "s"
-        if character.getPosition(offset=(0,-1,0)) == itemPos:
-            direction = "w"
-
-        return (None,(direction+"j","reset faction"))
-
     def generateTextDescription(self):
         return ["""
 The dungeons are too hard for you. 
@@ -90,21 +59,6 @@ You need to be stronger, to take them on.
 
 Get some upgrades to be stronger.
 """]
-
-    def assignToCharacter(self, character):
-        if self.character:
-            return
-
-        self.startWatching(character,self.wrapedTriggerCompletionCheck, "set faction")
-        super().assignToCharacter(character)
-
-    def wrapedTriggerCompletionCheck(self):
-        if self.completed:
-            1/0
-        if not self.active:
-            return
-
-        self.triggerCompletionCheck(self.character)
 
     def triggerCompletionCheck(self,character=None):
         if not character:

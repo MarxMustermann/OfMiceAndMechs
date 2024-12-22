296fcdc79d4313644dcba860b57f0f0cb562b9bb
MarxMustermann <marxmustermann@riseup.net>
2024-12-22 23:49:47 +0100

    added path tracking to adventure quest

--- src/questFolder/story/adventure.py
+++ src/questFolder/story/adventure.py
@@ -10,6 +10,8 @@ class Adventure(src.quests.MetaQuestSequence):
         self.metaDescription = description
         self.reason = reason
         self.visited_terrain = []
+        self.track = []
+        self.terrainInfo = {}
 
     def getNextStep(self,character=None,ignoreCommands=False, dryRun = True):
 
@@ -63,25 +65,60 @@ class Adventure(src.quests.MetaQuestSequence):
         return (None, None)
 
     def generateTextDescription(self):
-        return ["""
+        text = ["""
 Go out and adventure.
 
+track:
+
 """]
+        homeCoordinate = (self.character.registers["HOMETx"], self.character.registers["HOMETy"], 0)
+        characterCoordinate = self.character.getTerrain().getPosition()
+
+        rawMap = []
+        for y in range(15):
+            rawMap.append([])
+            for x in range(15):
+                if x == 0 or y == 0 or x == 14 or y == 14:
+                    rawMap[y].append("~~")
+                else:
+                    rawMap[y].append("  ")
+            rawMap[y].append("\n")
+
+        for (pos,info) in self.terrainInfo.items():
+            if info["tag"] == "nothingness":
+                rawMap[pos[1]][pos[0]] = src.canvas.displayChars.dirt
+            elif info["tag"] == "shrine":
+                rawMap[pos[1]][pos[0]] = (src.interaction.urwid.AttrSpec("#999", "black"),"\\/")
+            elif info["tag"] == "ruin":
+                rawMap[pos[1]][pos[0]] = (src.interaction.urwid.AttrSpec("#666", "black"),"&%")
+            else:
+                rawMap[pos[1]][pos[0]] = info["tag"][:2]
+        rawMap[homeCoordinate[1]][homeCoordinate[0]] = "HH"
+        rawMap[characterCoordinate[1]][characterCoordinate[0]] = "@@"
+
+        text.extend(rawMap)
+        text.extend("\n")
+        text.extend("0: Home base\n")
+    
+        counter = 1
+        for item in self.track:
+            text.append(f"{counter}: {item}\n")
+            counter += 1
+        return text
+
+    def handleChangedTerrain(self,extraInfo):
+        terrain = extraInfo["character"].getTerrain()
+        pos = terrain.getPosition()
+        tag = terrain.tag
+        self.track.append({"pos":pos,"tag":tag})
+        self.terrainInfo[pos] = {"tag":tag}
 
     def assignToCharacter(self, character):
         if self.character:
             return
 
-        self.startWatching(character,self.wrapedTriggerCompletionCheck, "set faction")
+        self.startWatching(character,self.handleChangedTerrain, "changedTerrain")
         super().assignToCharacter(character)
-
-    def wrapedTriggerCompletionCheck(self):
-        if self.completed:
-            1/0
-        if not self.active:
-            return
-
-        self.triggerCompletionCheck(self.character)
 
     def triggerCompletionCheck(self,character=None):
         if not character:

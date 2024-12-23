983614fbb4f9e47c1be56e8acaa0e376a2ece63b
MarxMustermann <marxmustermann@riseup.net>
2024-12-23 01:12:57 +0100

    added max health stat increase potion

--- src/itemFolder/Potion/__init__.py
+++ src/itemFolder/Potion/__init__.py
@@ -6,3 +6,4 @@ import src.itemFolder.Potion.IncreaseMaxHealthPotion
 import src.itemFolder.Potion.IncreaseHealthRegenPotion
 import src.itemFolder.Potion.PotionOfSpeed
 import src.itemFolder.Potion.AntiSlowPotion
+import src.itemFolder.Potion.PermaMaxHealthPotion

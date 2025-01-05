950126a9bde9e3c47c0702ef5fd275c04a0da291
MarxMustermann <marxmustermann@riseup.net>
2025-01-05 17:26:35 +0100

    gave clones better combat pathfinding

--- src/questFolder/fight.py
+++ src/questFolder/fight.py
@@ -117,7 +117,32 @@ So if an enemy is to directly east of you:
         if character.exhaustion > 1:
             return (None,(".","catch breath"))
 
-        # move toward enemies
+        # move toward enemies (smarter)
+        if character.rank:
+            shortestPath = None
+            for enemy in character.getNearbyEnemies():
+                if character.container.isRoom:
+                    path = character.container.getPathTile(character.getPosition(),enemy.getPosition(),ignoreEndBlocked=True,character=character)
+                else:
+                    path = character.container.getPathTile(character.getBigPosition(),character.getSpacePosition(),enemy.getSpacePosition(),ignoreEndBlocked=True,character=character)
+
+                if shortestPath == None or len(shortestPath) > len(path):
+                    shortestPath = path
+            if shortestPath:
+                command = None
+                step = shortestPath[0]
+                if step == (-1,0):
+                    command = "a"
+                if step == (1,0):
+                    command = "d"
+                if step == (0,-1):
+                    command = "w"
+                if step == (0,1):
+                    command = "s"
+
+                if command:
+                    return (None,(command,"approach enemy"))
+
         commands = []
         command = None
         for foundEnemy in character.getNearbyEnemies():

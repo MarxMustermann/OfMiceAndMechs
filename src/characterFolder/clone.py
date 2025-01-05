6ac0710bbf5541f83294827712241e51bca0356e
MarxMustermann <marxmustermann@riseup.net>
2025-01-05 17:00:04 +0100

    added a class for player characters

---
+++ src/characterFolder/clone.py
@@ -0,0 +1,38 @@
+
+import src
+
+class Clone(src.characters.Character):
+    """
+    the "human" player class
+    """
+
+    def __init__(
+        self,
+        display="@@",
+        xPosition=0,
+        yPosition=0,
+        quests=None,
+        automated=True,
+        name="Clone",
+        creator=None,
+        characterId=None,
+    ):
+        """
+        basic state setting
+        """
+        if quests is None:
+            quests = []
+        super().__init__(
+            display,
+            xPosition,
+            yPosition,
+            quests,
+            automated,
+            name,
+            creator=creator,
+            characterId=characterId,
+        )
+
+        self.charType = "Clone"
+
+src.characters.add_character(Clone)

903322676d25f1e8833175063bd4435498c6fe4e
MarxMustermann <marxmustermann@riseup.net>
2025-01-07 03:47:57 +0100

    made sovers generate inbetween steps

--- src/questFolder/gatherScrap.py
+++ src/questFolder/gatherScrap.py
@@ -98,7 +98,10 @@ Scrapfields are shown on the minimap as white ss"""]
                     if direction == (0,-1,0):
                         command = "Kw"
 
-                    return (None,(command*min(10-len(character.inventory),items[0].amount),"pick up scrap"))
+                    command = command*min(10-len(character.inventory),items[0].amount)
+                    if command[0] == "K" and "advancedPickup" in character.interactionState:
+                        command = command[1:]
+                    return (None,(command,"pick up scrap"))
 
         foundScrap = None
         room = character.container

e5390267e6de101dafd85329dcd443e65fd7b62f
MarxMustermann <marxmustermann@riseup.net>
2025-01-09 00:59:31 +0100

    fixed bug consuming too much goo flasks

--- src/itemFolder/functional/growthTank.py
+++ src/itemFolder/functional/growthTank.py
@@ -130,6 +130,9 @@ You talk to NPCs by pressing h and selecting the NPC to talk to.
                 self.gooCharges += flask.uses
                 character.addMessage(f"you fill the GrowthTank with {flask.uses} now it has {self.gooCharges}")
 
+                if self.gooCharges > 100:
+                    break
+
                 if flask in character.inventory:
                     character.inventory.remove(flask)
                     character.inventory.append(src.items.itemMap["Flask"]())

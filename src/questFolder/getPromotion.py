1f1c072c3dad173e2dfc2ac21ed604baaf72d766
MarxMustermann <marxmustermann@riseup.net>
2025-01-09 17:28:53 +0100

    made solver show inbetween steps

--- src/questFolder/getPromotion.py
+++ src/questFolder/getPromotion.py
@@ -52,6 +52,17 @@ Use the Promotor to do this.
         if character.macroState["submenue"]:
             return (None,(["esc"],"close the menu"))
 
+        if not character.container.isRoom:
+            pos = character.getSpacePosition()
+            if pos == (14,7,0):
+                return (None,("a","enter room"))
+            if pos == (0,7,0):
+                return (None,("d","enter room"))
+            if pos == (7,14,0):
+                return (None,("w","enter room"))
+            if pos == (7,0,0):
+                return (None,("s","enter room"))
+
         room = character.container
 
         if not isinstance(character.container, src.rooms.Room):

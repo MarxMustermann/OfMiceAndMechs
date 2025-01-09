3093415c554b899375ca148adb841ee6c3da1e1a
MarxMustermann <marxmustermann@riseup.net>
2025-01-09 17:29:37 +0100

    made solver show inbetween steps

--- src/questFolder/story/resetFaction.py
+++ src/questFolder/story/resetFaction.py
@@ -17,6 +17,17 @@ class ResetFaction(src.quests.MetaQuestSequence):
         if not character:
             return (None,None)
 
+        if not character.container.isRoom:
+            pos = character.getSpacePosition()
+            if pos == (14,7,0):
+                return (None,("a","enter room"))
+            if pos == (0,7,0):
+                return (None,("d","enter room"))
+            if pos == (7,14,0):
+                return (None,("w","enter room"))
+            if pos == (7,0,0):
+                return (None,("s","enter room"))
+
         if character.macroState.get("itemMarkedLast"):
             if character.macroState["itemMarkedLast"].type == "FactionSetter":
                 return (None,("j","reset faction"))

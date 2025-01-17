d2d8736b42bea5c87a2be96919bd090a8db4b91c
MarxMustermann <marxmustermann@riseup.net>
2025-01-17 15:20:47 +0100

    extended tests

--- tests/test_basic_items.py
+++ tests/test_basic_items.py
@@ -18,10 +18,9 @@ def anvil_room():
     room.addCharacter(character,2,2)
     anvil = src.items.itemMap["Anvil"]()
     room.addItem(anvil,(1,2,0))
-
     return (character,room)
 
-def test_anvil_1_from_inventory_to_inventory(anvil_room):
+def test_Anvil_1_from_inventory_to_inventory(anvil_room):
     character = anvil_room[0]
 
     scrap = src.items.itemMap["Scrap"]()
@@ -36,7 +35,7 @@ def test_anvil_1_from_inventory_to_inventory(anvil_room):
     assert len(character.inventory) == 1
     assert character.inventory[0].type == "MetalBars"
 
-def test_anvil_10_from_inventory_to_inventory(anvil_room):
+def test_Anvil_10_from_inventory_to_inventory(anvil_room):
     character = anvil_room[0]
 
     for i in range(10):
@@ -53,7 +52,7 @@ def test_anvil_10_from_inventory_to_inventory(anvil_room):
     for item in character.inventory:
         assert item.type == "MetalBars"
 
-def test_anvil_show_schedule(anvil_room):
+def test_Anvil_show_schedule(anvil_room):
     character = anvil_room[0]
 
     character.runCommandString("Jassj")
@@ -62,10 +61,28 @@ def test_anvil_show_schedule(anvil_room):
         character.timeTaken = 0
         character.advance(advanceMacros=True)
 
-def test_anvil_schedule_production(anvil_room):
+def test_Anvil_schedule_production(anvil_room):
     character = anvil_room[0]
 
     character.runCommandString("Jasssj")
+
+    for i in range(10):
+        character.timeTaken = 0
+        character.advance(advanceMacros=True)
+
+@pytest.fixture
+def metalWorking_room():
+    room = src.rooms.EmptyRoom()
+    character = src.characters.characterMap["Clone"]()
+    room.addCharacter(character,2,2)
+    item = src.items.itemMap["MetalWorkingBench"]()
+    room.addItem(item,(1,2,0))
+    return (character,room)
+
+def test_MetalWorkingBench_schedule_production(metalWorking_room):
+    character = metalWorking_room[0]
+
+    character.runCommandString("Jajj")
 
     for i in range(10):
         character.timeTaken = 0

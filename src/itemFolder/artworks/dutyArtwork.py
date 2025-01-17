ae35127e6c42587a44c6c163a18c3262ec69128f
MarxMustermann <marxmustermann@riseup.net>
2025-01-17 03:21:34 +0100

    removed dead code

--- src/itemFolder/artworks/dutyArtwork.py
+++ src/itemFolder/artworks/dutyArtwork.py
@@ -19,16 +19,12 @@ class DutyArtwork(src.items.Item):
 
         self.applyOptions.extend(
                                                 [
-                                                    ("showOverview", "show overview"),
                                                     ("showMatrix", "show matrix based"),
-                                                    ("changeOwnDuties", "change own duties"),
                                                 ]
                                 )
 
         self.applyMap = {
                                     "showMatrix": self.showMatrix,
-                                    "showOverview": self.showOverview,
-                                    "changeOwnDuties": self.changeOwnDuties,
                                 }
 
         self.description = """
@@ -39,57 +35,8 @@ This will change what work the clones are doing when told to be useful."""
 Use it by activating it and selecting in what mode you want to set the duties.
 After changing the duties the clones should change their behaviour after completing their current task."""
 
-    def changeOwnDuties(self,character):
-        self.changeOwnDuties_real({"character":character})
-
-    def changeOwnDuties_real(self,params):
-        character = params["character"]
-
-        if params.get("duty") == "None":
-            return
-        return
-
-        options = []
-        options.append(("None","exit menu"))
-        options.append(("duty by name","duty by name"))
-        options.append(("duty1","duty1"))
-        options.append(("duty2","duty2"))
-
-        submenue = src.menuFolder.selectionMenu.SelectionMenu("select duty to toggle\n",options,targetParamName="duty")
-        character.macroState["submenue"] = submenue
-        character.macroState["submenue"].followUp = {"container":self,"method":"changeOwnDuties_real","params":params}
-
-        pass
-
     def changeCharges(self,delta):
         self.charges += delta
-
-    def showOverview(self, character):
-        text = ""
-
-        terrain = character.getTerrain()
-
-        characters = []
-        characters.extend(terrain.characters)
-        for room in terrain.rooms:
-            characters.extend(room.characters)
-
-        for testChar in characters[:]:
-            if testChar.faction != character.faction or len(testChar.duties) > 1:
-                characters.remove(testChar)
-
-        dutyMap = {}
-        for char in characters:
-            for duty in char.duties:
-                if duty not in dutyMap:
-                    dutyMap[duty] = 0
-                dutyMap[duty] += 1
-
-        for (k,v) in dutyMap.items():
-            text += f"{k} - {v}\n"
-
-        submenue = src.menuFolder.textMenu.TextMenu(text)
-        character.macroState["submenue"] = submenue
 
     def showMatrix(self, character):
 

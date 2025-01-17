e1a525f956ba0b26324e940a9271f3901ee0db09
MarxMustermann <marxmustermann@riseup.net>
2025-01-17 16:45:12 +0100

    extended tests

--- tests/test_basic_items.py
+++ tests/test_basic_items.py
@@ -46,11 +46,25 @@ def test_simple_apply(itemType,character_room):
         character.timeTaken = 0
         character.advance(advanceMacros=True)
 
+@pytest.mark.parametrize("itemType", src.items.itemMap.values())
+def test_simple_configure(itemType,character_room):
+    if itemType.isAbstract:
+        return
+
+    (character,room) = character_room
+
+    item = itemType()
+
+    room.addItem(item,(1,2,0))
+
+    character.runCommandString("Ca")
+    for i in range(10):
+        character.timeTaken = 0
+        character.advance(advanceMacros=True)
+
 @pytest.fixture
-def anvil_room():
-    room = src.rooms.EmptyRoom()
-    character = src.characters.characterMap["Clone"]()
-    room.addCharacter(character,2,2)
+def anvil_room(character_room):
+    (character,room) = character_room
     anvil = src.items.itemMap["Anvil"]()
     room.addItem(anvil,(1,2,0))
     return (character,room)
@@ -106,19 +120,23 @@ def test_Anvil_schedule_production(anvil_room):
         character.advance(advanceMacros=True)
 
 @pytest.fixture
-def metalWorking_room():
-    room = src.rooms.EmptyRoom()
-    character = src.characters.characterMap["Clone"]()
-    room.addCharacter(character,2,2)
+def metalWorking_room(character_room):
+    (character,room) = character_room
     item = src.items.itemMap["MetalWorkingBench"]()
     room.addItem(item,(1,2,0))
     return (character,room)
 
-def test_MetalWorkingBench_schedule_production(metalWorking_room):
+def test_MetalWorkingBench_production(metalWorking_room):
     character = metalWorking_room[0]
+
+    item = src.items.itemMap["MetalBars"]()
+    character.inventory.append(item)
 
     character.runCommandString("Jajj")
 
-    for i in range(10):
+    for i in range(1000):
         character.timeTaken = 0
         character.advance(advanceMacros=True)
+
+    assert len(character.inventory) == 1
+    assert character.inventory[0].type != "MetalBars"

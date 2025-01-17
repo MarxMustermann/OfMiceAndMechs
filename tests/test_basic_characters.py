a4f3ca3d2525a949c35a9ae4d6bcb50c6a6ac188
MarxMustermann <marxmustermann@riseup.net>
2025-01-17 03:59:17 +0100

    added simple test for movement

--- tests/test_basic_characters.py
+++ tests/test_basic_characters.py
@@ -7,3 +7,30 @@ def test_creation():
 
     for characterType in src.characters.characterMap.values():
         character = characterType()
+
+def test_movement():
+    room = src.rooms.EmptyRoom()
+    character = src.characters.characterMap["Clone"]()
+    room.addCharacter(character,2,2)
+    assert character.xPosition == 2
+    assert character.yPosition == 2
+    character.timeTaken = 0
+    character.runCommandString("d")
+    character.advance(advanceMacros=True)
+    assert character.xPosition == 3
+    assert character.yPosition == 2
+    character.timeTaken = 0
+    character.runCommandString("s")
+    character.advance(advanceMacros=True)
+    assert character.xPosition == 3
+    assert character.yPosition == 3
+    character.timeTaken = 0
+    character.runCommandString("a")
+    character.advance(advanceMacros=True)
+    assert character.xPosition == 2
+    assert character.yPosition == 3
+    character.timeTaken = 0
+    character.runCommandString("w")
+    character.advance(advanceMacros=True)
+    assert character.xPosition == 2
+    assert character.yPosition == 2

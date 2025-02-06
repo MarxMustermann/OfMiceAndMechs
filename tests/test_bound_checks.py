aa5a6441bfcf79f30e3580fbc527388fa1420ca3
MarxMustermann <marxmustermann@riseup.net>
2025-02-06 05:18:39 +0100

    added tests for bound checks

---
+++ tests/test_bound_checks.py
@@ -0,0 +1,55 @@
+import src
+import pytest
+
+src.canvas.displayChars = src.canvas.DisplayMapping("pureASCII")
+src.interaction.urwid = src.pseudoUrwid
+src.gamestate.gamestate = src.gamestate.GameState(11)
+
+def test_gotoPosition_bounds(character_room):
+    src.quests.questMap["GoToPosition"](targetPosition=(9,9,0))
+
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToPosition"](targetPosition=(9,0,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToPosition"](targetPosition=(0,9,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToPosition"](targetPosition=(14,9,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToPosition"](targetPosition=(9,14,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToPosition"](targetPosition=(9,5000,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToPosition"](targetPosition=(5000,14,0))
+
+def test_gotoTile(character_room):
+    src.quests.questMap["GoToTile"](targetPosition=(9,9,0))
+
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToTile"](targetPosition=(9,0,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToTile"](targetPosition=(0,9,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToTile"](targetPosition=(14,9,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToTile"](targetPosition=(9,14,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToTile"](targetPosition=(9,5000,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToTile"](targetPosition=(5000,14,0))
+
+def test_gotoTerrain(character_room):
+    src.quests.questMap["GoToTerrain"](targetTerrain=(9,9,0))
+
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToTerrain"](targetTerrain=(9,0,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToTerrain"](targetTerrain=(0,9,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToTerrain"](targetTerrain=(14,9,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToTerrain"](targetTerrain=(9,14,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToTerrain"](targetTerrain=(9,5000,0))
+    with pytest.raises(ValueError):
+        src.quests.questMap["GoToTerrain"](targetTerrain=(5000,14,0))
+

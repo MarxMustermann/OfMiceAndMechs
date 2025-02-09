3117551ad5d43a08903ad3007613599d39f19f31
MarxMustermann <marxmustermann@riseup.net>
2025-02-09 15:46:09 +0100

    extended fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,26 @@ import src
 import pytest
 
 @pytest.fixture
-def character_room():
-    room = src.rooms.EmptyRoom()
+def terrain():
+    terrain = src.terrains.Nothingness()
+    terrain.xPosition = 7
+    terrain.yPosition = 7
+    return terrain
+
+@pytest.fixture
+def character():
     character = src.characters.characterMap["Clone"]()
-    room.addCharacter(character,2,2)
     character.registers["HOMETx"] = 7
     character.registers["HOMETy"] = 7
     character.registers["HOMEx"] = 7
     character.registers["HOMEy"] = 7
+
+    return character
+
+@pytest.fixture
+def character_room(terrain,character):
+    room = src.rooms.EmptyRoom()
+    room.addCharacter(character,2,2)
 
     room.xPosition = 7
     room.yPosition = 7
@@ -18,10 +30,7 @@ def character_room():
     room.hidden = False
     room.reconfigure(15, 15, doorPos=[])
 
-    terrain = src.terrains.Nothingness()
     terrain.addRooms([room])
-    terrain.xPosition = 7
-    terrain.yPosition = 7
 
     return (character,room)
 

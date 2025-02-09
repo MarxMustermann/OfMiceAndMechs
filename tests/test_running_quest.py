65a32249855343b75f593a013432b0bc8085e650
MarxMustermann <marxmustermann@riseup.net>
2025-02-09 22:57:56 +0100

    moved debugging unit test to proper files

--- tests/test_running_quest.py
+++ tests/test_running_quest.py
@@ -18,3 +18,52 @@ def test_dutyhangup_painting_no_painter(character_room):
         character.timeTaken = 0
         character.advance(advanceMacros=True)
 
+def test_movement_working_GoToPosition(terrain,character):
+
+    bigPos = (6,10,0)
+
+    terrain.addCharacter(character,bigPos[0]*15+3,bigPos[1]*15+7)
+
+    assert character.getBigPosition() == bigPos
+    assert character.getSpacePosition() == (3,7,0)
+
+    quest = src.quests.questMap["GoToPosition"](targetPosition=(7,1,0))
+    character.assignQuest(quest)
+
+    character.runCommandString("*")
+
+    for i in range(50):
+        character.timeTaken = 0
+        character.advance(advanceMacros=True)
+        assert character.getBigPosition() == bigPos
+
+    assert character.getBigPosition() == bigPos
+    assert character.getSpacePosition() == (7,1,0)
+
+def test_movement_blocked_GoToPosition(terrain,character):
+
+    bigPos = (6,10,0)
+    targetPos = (7,1,0)
+
+    terrain.addCharacter(character,bigPos[0]*15+3,bigPos[1]*15+7)
+
+    for x in range(1,14):
+        scrap = src.items.itemMap["Scrap"](amount=7)
+        terrain.addItem(scrap,(bigPos[0]*15+x,bigPos[1]*15+2,0))
+
+    quest = src.quests.questMap["GoToPosition"](targetPosition=targetPos)
+    character.assignQuest(quest)
+
+    assert character.quests == [quest]
+
+    character.runCommandString("*")
+
+    character.advance(advanceMacros=True)
+
+    assert character.quests == []
+
+def test_movement_working_GoToTile(terrain,character):
+
+    bigPos = (6,10,0)
+    targetBigPos = (6,9,0)
+

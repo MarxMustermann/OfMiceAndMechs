69f5cf2a0dceb8bb583d7afceca5d91a41102274
MarxMustermann <marxmustermann@riseup.net>
2025-05-10 02:14:11 +0200

    commented code

--- src/itemFolder/workshops/armorReinforcer.py
+++ src/itemFolder/workshops/armorReinforcer.py
@@ -3,6 +3,10 @@ import random
 import src
 
 class ArmorReinforcer(src.items.itemMap["WorkShop"]):
+    """
+    ingame item to uprage armor
+    """
+
     type = "ArmorReinforcer"
     name = "armor Reinforcer"
     description = "Use it to upgrade armors"
@@ -11,15 +15,26 @@ class ArmorReinforcer(src.items.itemMap["WorkShop"]):
 
     def __init__(self):
         super().__init__(display="AR")
+
+        # configure interaction menu
         self.applyOptions.extend([("Reinforce Armor", "Reinforce Armor")])
         self.applyMap = {"Reinforce Armor": self.reinforceArmorHook}
 
     def reinforceArmorHook(self, character):
+        """
+        indirection to call the actual function
+        """
         self.reinforceArmor({"character": character})
 
     def reinforceArmor(self, params):
+        """
+        start upgrading the armor
+        """
+
+        # unpack the parameters
         character = params["character"]
 
+        # get user input on what armor to upgrade
         if "choice" not in params:
             options = [("Reinforce Equipped Armor", "Reinforce Equipped Armor"), ("Reinforce Armor", "Reinforce Armor")]
             submenue = src.menuFolder.selectionMenu.SelectionMenu(
@@ -30,15 +45,18 @@ class ArmorReinforcer(src.items.itemMap["WorkShop"]):
             character.macroState["submenue"].followUp = {"container": self, "method": "reinforceArmor", "params": params}
             return
 
+        # get available chitin plates
         chitinPlates = []
         for item in character.inventory:
             if not isinstance(item, src.items.itemMap["ChitinPlates"]):
                 continue
             chitinPlates.append(item)
 
+        # show warning message when upgrade items are missing
         if not chitinPlates:
             character.addMessage("you don't have ChitinPlates, you need ChitinPlates to upgrade your Armor up to more than 3")
 
+        # get the armor to upgrade
         armor = None
         if params["choice"] == "Reinforce Equipped Armor":
             if character.armor:
@@ -55,39 +73,43 @@ class ArmorReinforcer(src.items.itemMap["WorkShop"]):
                 character.addMessage("you don't have any Armor in the inventory")
                 return
 
+        # calculate how much to improve the armor by
         improvementAmount = 0
         if armor.armorValue < 3:
             improvementAmount += 3-armor.armorValue
-
         for chitinPlate in chitinPlates:
             if improvementAmount+armor.armorValue >= 8:
                 break
-
             character.inventory.remove(chitinPlate)
             improvementAmount += min(0.5,8-armor.armorValue-improvementAmount)
-
         if not improvementAmount:
             character.changed("improved armor")
             character.addMessage("you can't improve your armor")
             return
 
+        # trigger a waiting period before completing upgrading the armor
         params["Armor"] = armor
         params["productionTime"] = int(100*improvementAmount)+1
         params["doneProductionTime"] = 0
         params["hitCounter"] = character.numAttackedWithoutResponse
         params["improvementAmount"] = improvementAmount
-
         self.produceItem_wait(params)
 
     def produceItem_done(self, params):
+        """
+        Finalize improving the armor
+        """
+
+        # unpack parameters
         character = params["character"]
         improvement = params["improvementAmount"]
+        armor = params["Armor"]
 
+        # trigger events
         character.changed("improved armor")
 
-        armor = params["Armor"]
+        # actually improve the armor
         armor.armorValue += improvement
-
         character.addMessage(f"You improved the Armor by {improvement!s} to {armor.armorValue}")
 
 

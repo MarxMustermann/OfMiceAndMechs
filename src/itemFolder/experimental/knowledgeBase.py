c7f96c016dcbe676e9b5707af0131903f43ee9ba
MarxMustermann <marxmustermann@riseup.net>
2025-05-17 00:19:45 +0200

    made item boltable

--- src/itemFolder/experimental/knowledgeBase.py
+++ src/itemFolder/experimental/knowledgeBase.py
@@ -12,9 +12,6 @@ class KnowledgeBase(src.items.Item):
     def apply(self, character):
         self.show_info({"character":character,"search_term":""})
 
-    def configure(self, character):
-        self.show_info({"character":character,"search_term":""})
-
     def show_info(self, params):
 
         character = params["character"]
@@ -43,6 +40,31 @@ class KnowledgeBase(src.items.Item):
         submenu.followUp = {"container":self,"method":"show_info","params":params}
         character.macroState["submenue"] = submenu
 
+    def getConfigurationOptions(self, character):
+        """
+        register the configuration options with superclass
+
+        Parameters:
+            character: the character trying to conigure the machine
+        """
+
+        options = super().getConfigurationOptions(character)
+        if self.bolted:
+            options["b"] = ("unbolt", self.unboltAction)
+        else:
+            options["b"] = ("bolt down", self.boltAction)
+        return options
+
+    def boltAction(self,character):
+        self.bolted = True
+        character.addMessage("you bolt down the KnowledgeBase")
+        character.changed("boltedItem",{"character":character,"item":self})
+
+    def unboltAction(self,character):
+        self.bolted = False
+        character.addMessage("you unbolt the KnowledgeBase")
+        character.changed("unboltedItem",{"character":character,"item":self})
+
     def getLongInfo(self):
         return """
 item: Spawner
@@ -52,6 +74,5 @@ spawner with %s charges
 """ % (
             self.charges
         )
-
 
 src.items.addType(KnowledgeBase)

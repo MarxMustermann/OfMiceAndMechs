79e601eca5d660996a761a4c44e97e70b5c7613d
MarxMustermann <marxmustermann@riseup.net>
2025-06-08 17:16:54 +0200

    commented and cleaned up code

--- src/itemFolder/workshops/swordSharpener.py
+++ src/itemFolder/workshops/swordSharpener.py
@@ -18,8 +18,11 @@ class SwordSharpener(src.items.itemMap["WorkShop"]):
         self.sharpenSword({"character": character})
 
     def sharpenSword(self, params):
+
+        # unnpack paramters
         character = params["character"]
 
+        # make the user select the 
         if "choice" not in params:
             options = [("Sharpen Equipped Sword", "Sharpen Equipped Sword"), ("Sharpen Sword", "Sharpen Sword")]
             submenue = src.menuFolder.selectionMenu.SelectionMenu(
@@ -30,15 +33,18 @@ class SwordSharpener(src.items.itemMap["WorkShop"]):
             character.macroState["submenue"].followUp = {"container": self, "method": "sharpenSword", "params": params}
             return
 
+        # check the grindstones that can be used to upgrade the sword
         grindstones = []
         for item in character.inventory:
             if not isinstance(item, src.items.itemMap["Grindstone"]):
                continue 
             grindstones.append(item)
 
+        # warn the player that only basic upgrades can be done without grindstones
         if not grindstones:
             character.addMessage("you don't have Grindstone, you only can improve your sword to 15")
 
+        # get the sword to sharpen
         sword = None
         if params["choice"] == "Sharpen Equipped Sword":
             if character.weapon:
@@ -55,15 +61,18 @@ class SwordSharpener(src.items.itemMap["WorkShop"]):
                 character.addMessage("you don't have any base sword in the inventory")
                 return
 
-        improvementAmount = 0
-        grindstone_consumed = 0
-        if sword.baseDamage < 15:
-            improvementAmount += 15-sword.baseDamage
-
-        elif sword.baseDamage == 30:
+        # abort and notify the user if the sword is upgraded to the maximum already
+        if sword.baseDamage == 30:
             character.addMessage("you can't further improve the sword")
             return
 
+        # get improvement amount that has no costs associated
+        improvementAmount = 0
+        if sword.baseDamage < 15:
+            improvementAmount += 15-sword.baseDamage
+
+        # calculate how much improvement can be done using grindstones
+        grindstone_consumed = 0
         while grindstone_consumed <= len(grindstones):
             current_damage_output = sword.baseDamage + improvementAmount
             if current_damage_output < 25:
@@ -79,23 +88,24 @@ class SwordSharpener(src.items.itemMap["WorkShop"]):
             else:
                 break
 
+        # abort and notify user if sword can't be improved
+        if not improvementAmount:
+            character.addMessage("you can't improve your sword. You need more Grindstone")
+            character.changed("sharpened sword")
+            return
+
+        # remove/destroy grindstones used to upgrade sword
         if grindstone_consumed:
             for grindStone in grindstones[:grindstone_consumed]:
                 character.inventory.remove(grindStone)
 
-        if not improvementAmount:
-            character.addMessage("you can't improve your sword")
-            character.changed("sharpened sword")
-            return
-
+        # trigger the actual productions process
         params["sword"] = sword
         params["productionTime"] = 20*improvementAmount
         params["doneProductionTime"] = 0
         params["improvementAmount"] = improvementAmount
         params["cost"] = grindstone_consumed
         params["hitCounter"] = character.numAttackedWithoutResponse
-
-
         self.produceItem_wait(params)
 
     def produceItem_done(self, params):

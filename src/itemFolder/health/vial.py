18f096afa52776780d0b5d64bbcb918ff816f920
MarxMustermann <marxmustermann@riseup.net>
2025-07-06 00:34:43 +0200

    tweaked docstrings

--- src/itemFolder/health/vial.py
+++ src/itemFolder/health/vial.py
@@ -2,17 +2,11 @@ import src
 
 
 class Vial(src.items.Item):
-    """
+    '''
     ingame item with health to carry around and drink from
-    """
-
+    '''
     type = "Vial"
-
     def __init__(self, name="vial", noId=False):
-        """
-        configure super class
-        """
-
         super().__init__(display=src.canvas.displayChars.gooflask_empty, name=name)
         self.walkable = True
         self.bolted = False
@@ -23,12 +17,12 @@ class Vial(src.items.Item):
         self.level = 1
 
     def apply(self, character):
-        """
-        handle a character trying to drink from the flask
+        '''
+        handle a character trying to drink from the vial
 
         Parameters:
             character: the character trying to use the item
-        """
+        '''
 
         # handle edge case
         if self.uses <= 0:
@@ -50,12 +44,12 @@ class Vial(src.items.Item):
         character.changed()
 
     def render(self):
-        """
+        '''
         render based on fill amount
 
         Returns:
             what the item should look like
-        """
+        '''
 
         displayByUses = [
             src.canvas.displayChars.vial_empty,
@@ -69,22 +63,22 @@ class Vial(src.items.Item):
 
 
     def getDetailedInfo(self):
-        """
+        '''
         get info including the charges on the flask
 
         Returns:
             the description text
-        """
+        '''
 
         return super().getDetailedInfo() + " (" + str(self.uses) + " charges)"
 
     def getLongInfo(self):
-        """
+        '''
         returns a longer than normal description text
 
         Returns:
             the description text
-        """
+        '''
 
         text = super().getLongInfo()
 
@@ -97,20 +91,20 @@ this is a level {self.level} item.
         return text
 
     def upgrade(self):
-        """
+        '''
         increase max uses when upgraded
-        """
+        '''
         super().upgrade()
 
         self.maxUses += 1
 
     def downgrade(self):
-        """
+        '''
         increase max uses when downgraded
-        """
+        '''
         super().downgrade()
 
         self.maxUses -= 1
 
-
+# register then item type
 src.items.addType(Vial)

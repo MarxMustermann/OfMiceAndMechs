18724fa28e55fd9ea868d22cf909652e2d8ae846
MarxMustermann <marxmustermann@riseup.net>
2025-07-19 15:35:35 +0200

    added secondary use to memory fragments

--- src/itemFolder/story/memoryFragment.py
+++ src/itemFolder/story/memoryFragment.py
@@ -1,4 +1,5 @@
 import src
+import random
 
 
 class MemoryFragment(src.items.Item):
@@ -10,6 +11,26 @@ class MemoryFragment(src.items.Item):
         super().__init__(display="mf", name="Memory Fragment")
         self.walkable = True
         self.bolted = False
+
+    def apply(self,character):
+        '''
+        uncover some terrain as secondary usage
+        '''
+
+        # uncover random position
+        pos = (random.randint(1,13),random.randint(1,13))
+        currentInfo = character.terrainInfo.get(pos)
+        terrain = src.gamestate.gamestate.terrainMap[pos[1]][pos[0]]
+        if not currentInfo:
+            character.terrainInfo[terrain.getPosition()] = {"tag":terrain.tag}
+            text = f"you uncovered the terrain {pos}.\nIt is a {terrain.tag}"
+        else:
+            character.terrainInfo[terrain.getPosition()] = {"tag":terrain.tag}
+            text = f"you uncovered the terrain {pos}.\nIt is already known"
+        character.addMessage(text)
+
+        # self destruct
+        self.destroy()
 
 # register the item class
 src.items.addType(MemoryFragment, nonManufactured=True)

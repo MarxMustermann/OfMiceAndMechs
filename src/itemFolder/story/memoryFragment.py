5492fbe9469138d4d286c7c9a491e32bc9905bee
MarxMustermann <marxmustermann@riseup.net>
2025-07-19 14:09:47 +0200

    added docstrings and comments

--- src/itemFolder/story/memoryFragment.py
+++ src/itemFolder/story/memoryFragment.py
@@ -2,12 +2,14 @@ import src
 
 
 class MemoryFragment(src.items.Item):
+    '''
+    ingame item mainy used as ressource to buy reports
+    '''
     type = "MemoryFragment"
-
     def __init__(self):
         super().__init__(display="mf", name="Memory Fragment")
         self.walkable = True
         self.bolted = False
 
-
+# register the item class
 src.items.addType(MemoryFragment, nonManufactured=True)

a01a0ccce0002334dfecfa271aa9bae4bedc6087
hero2002 <abdkafi2002@gmail.com>
2025-07-29 21:51:35 +0300

    improved AddedStatusEffect popup Massage

--- src/popupFolder/addedStatusEffect.py
+++ src/popupFolder/addedStatusEffect.py
@@ -1,3 +1,4 @@
+import re
 from functools import partial
 
 import src
@@ -6,22 +7,24 @@ import src.statusEffects
 
 
 class AddedStatusEffect(src.popups.Popup):
-    def __init__(self, effectType, message):
-        self.effectType = effectType
-        self.message = message
+    def __init__(self, effect):
+        self.effect = effect
         super().__init__()
 
     def subscribedEvent(self):
         return "added status effect"
 
     def text(self):
-        return self.message
+        status_name = re.sub(r"(?<=[a-z])(?=[A-Z])", " ", self.effect.type)
+        instance = self.effect()
+        short_name = instance.getShortCode()
+        description = instance.getLoreDescription()
+
+        return f"you gained a new status effect: {status_name}({short_name})\n\n{description}"
 
     def conditionMet(self, params) -> bool:
-        return self.character.statusEffects[-1].type == self.effectType
+        return self.character.statusEffects[-1].type == self.effect.type
 
 
-statusEffect_message = [(key, e().getLoreDescription()) for (key, e) in src.statusEffects.statusEffectMap.items()]
-
-for type, message in statusEffect_message:
-    src.popups.popupsArray.append(partial(AddedStatusEffect, type, message))
+for _, type in src.statusEffects.statusEffectMap.items():
+    src.popups.popupsArray.append(partial(AddedStatusEffect, type))

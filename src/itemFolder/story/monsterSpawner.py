53aa82f9928cdab2b5c368e25bee49d34a8c2cce
MarxMustermann <marxmustermann@riseup.net>
2025-08-01 01:39:12 +0200

    made clone lab spawn different strength bosses

--- src/itemFolder/story/monsterSpawner.py
+++ src/itemFolder/story/monsterSpawner.py
@@ -1,20 +1,29 @@
 import src
-
+import random
 
 class MonsterSpawner(src.items.Item):
     type = "MonsterSpawner"
-    def __init__(self):
+    def __init__(self,strength=None):
         super().__init__(display=src.canvas.displayChars.sparkPlug)
         self.name = "monster spawner"
+
+        if strength is None:
+            strength = random.randint(1,10)
 
         self.walkable = False
         self.bolted = True
         self.disabled = False
+        self.strength = 1
 
     def apply(self, character):
+        try:
+            self.strength
+        except:
+            self.strength = random.randint(1,10)
+
         pos = self.getPosition()
         room = self.container
-        enemy = src.characters.characterMap["Guardian"](modifier=1)
+        enemy = src.characters.characterMap["Guardian"](modifier=self.strength)
         room.addCharacter(enemy,pos[0],pos[1])
 
         quest = src.quests.questMap["ClearTerrain"]()

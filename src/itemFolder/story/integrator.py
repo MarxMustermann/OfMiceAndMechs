2219f52d59836657ba782be099845db945cc5fb5
MarxMustermann <marxmustermann@riseup.net>
2025-08-03 01:56:53 +0200

    made NPCs spawn with random duty priotities

--- src/itemFolder/story/integrator.py
+++ src/itemFolder/story/integrator.py
@@ -2,9 +2,9 @@ import src
 import random
 
 class Integrator(src.items.Item):
-    """
+    '''
     ingame item to set to baic values a NPC needs to work in a base
-    """
+    '''
     type = "Integrator"
     def __init__(self,epoch=0):
         super().__init__(display="IT")
@@ -14,16 +14,22 @@ class Integrator(src.items.Item):
         self.bolted = True
 
     def apply(self,character):
+        '''
+        set the duties for the character using this item
+        '''
+
+        # show user feedback
         character.addMessage(f"your duties were set. be useful!")
 
+        # add quest to activate AI
         quest = src.quests.questMap["BeUsefull"](strict=True)
         quest.autoSolve = True
         quest.assignToCharacter(character)
         quest.activate()
         character.assignQuest(quest,active=True)
-        character.foodPerRound = 1
 
-        terrain = character.getTerrain()
+        # make charcter consume food (hack?)
+        character.foodPerRound = 1
 
         duties = [ "room building",
                    "resource gathering",
@@ -42,18 +48,17 @@ class Integrator(src.items.Item):
                    "praying" ]
         character.duties = duties
 
-        character.dutyPriorities["praying"] = 4
-        character.dutyPriorities["room building"] = 2
-        character.dutyPriorities["painting"] = 2
+        for duty in duties:
+            character.dutyPriorities[duty] = random.randint(1,4)
 
+        terrainPos = character.getTerrain().getPosition()
         characterPos = character.getBigPosition()
         character.registers["HOMEx"] = characterPos[0]
         character.registers["HOMEy"] = characterPos[1]
-        character.registers["HOMETx"] = terrain.xPosition
-        character.registers["HOMETy"] = terrain.yPosition
+        character.registers["HOMETx"] = terrainPos[0]
+        character.registers["HOMETy"] = terrainPos[1]
 
         self.container.addAnimation(character.getPosition(),"showchar",1,{"char":"OO"})
         self.container.addAnimation(self.getPosition(),"showchar",1,{"char":"OO"})
-
 
 src.items.addType(Integrator)

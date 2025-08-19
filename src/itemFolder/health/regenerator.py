03cd3f06de6ca59fa46e5b13359fc0153f881c1d
MarxMustermann <marxmustermann@riseup.net>
2025-08-20 01:26:44 +0200

    allowed by to heal by using the regenerator

--- src/itemFolder/health/regenerator.py
+++ src/itemFolder/health/regenerator.py
@@ -34,6 +34,22 @@ class Regenerator(src.items.Item):
 
         # do nothing when already running
         if self.activated:
+            text = "your place your hand on the Regenerator" 
+            if character.health >= character.maxHealth:
+                text += ", but you do not need healing."
+            elif not self.mana_charges:
+                text += ", but the Regenerator has no charges left."
+            else:
+                heal_amount = min(self.healing_amount*self.mana_charges,character.maxHealth-character.health)
+                character.heal(heal_amount, reason="the Regenerator heals you")
+                text += f" and it heals you for {heal_amount} HP."
+                self.mana_charges -= heal_amount//self.healing_amount
+                if heal_amount%self.healing_amount:
+                    self.mana_charges -= 1
+                text += f"\nThe regenerator now has {self.mana_charges} charges left."
+            submenue = src.menuFolder.textMenu.TextMenu(text)
+            character.macroState["submenue"] = submenue
+            character.runCommandString("~",nativeKey=True)
             return
 
         # trigger the loop of healing events

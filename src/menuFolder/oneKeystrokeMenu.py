91ab5bf5a0bb90577ede067462d854f419380de8
MarxMustermann <marxmustermann@riseup.net>
2025-08-24 20:25:36 +0200

    tweaked docstrings and comments

--- src/menuFolder/oneKeystrokeMenu.py
+++ src/menuFolder/oneKeystrokeMenu.py
@@ -1,20 +1,13 @@
 import src
 
 class OneKeystrokeMenu(src.subMenu.SubMenu):
-    """
+    '''
     a menu getting a single keystroke from the character
-    """
-
+    Parameters:
+        text: the text to show
+    '''
     type = "OneKeystrokeMenu"
-
     def __init__(self, text="",targetParamName="keyPressed"):
-        """
-        initialise inernal state
-
-        Parameters:
-            text: the text to show
-        """
-
         super().__init__()
         self.text = text
         self.firstRun = True
@@ -25,11 +18,14 @@ class OneKeystrokeMenu(src.subMenu.SubMenu):
         self.rerenderFunction = None
 
     def rerender(self):
+        '''
+        show the last state again
+        '''
         if self.rerenderFunction:
             src.interaction.main.set_text((src.interaction.urwid.AttrSpec("default", "default"), self.rerenderFunction()))
 
     def handleKey(self, key, noRender=False, character = None):
-        """
+        '''
         show the text and quit on second keypress
 
         Parameters:
@@ -37,7 +33,7 @@ class OneKeystrokeMenu(src.subMenu.SubMenu):
             noRender: flag to skip rendering
         Returns:
             returns True when done
-        """
+        '''
 
         # show info
         if not noRender:
@@ -53,6 +49,6 @@ class OneKeystrokeMenu(src.subMenu.SubMenu):
             self.done = True
             return True
 
+        # mark that the menu was shown once
         self.firstRun = False
-
         return False

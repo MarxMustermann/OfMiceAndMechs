04e2f4c818e1c45f3abcbe5badbf292cb514060d
MarxMustermann <marxmustermann@riseup.net>
2025-08-27 11:53:20 +0200

    commented code

--- src/itemFolder/potionsFolder/buffPotion.py
+++ src/itemFolder/potionsFolder/buffPotion.py
@@ -4,10 +4,16 @@ import src
 
 
 class BuffPotion(src.items.itemMap["Potion"], ABC):
+    '''
+    abstract class for ingame items that apply buffs to the player
+    '''
     type = "BuffPotion"
     isAbstract = True
 
     def apply(self, character):
+        '''
+        apply the buffs to the character
+        '''
         character.statusEffects.extend(self.getBuffsToAdd())
         character.changed("new status effect")
         super().apply(character)
@@ -15,5 +21,5 @@ class BuffPotion(src.items.itemMap["Potion"], ABC):
     @abstractmethod
     def getBuffsToAdd(self): ...
 
-
+# register the item type (should it be really?)
 src.items.addType(BuffPotion)

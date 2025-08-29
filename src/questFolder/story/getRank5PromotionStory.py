e1cdb27cd37c6395510856a757745ff28aaf9b52
MarxMustermann <marxmustermann@riseup.net>
2025-08-29 02:07:11 +0200

    removed inactive code

--- src/questFolder/story/getRank5PromotionStory.py
+++ src/questFolder/story/getRank5PromotionStory.py
@@ -85,44 +85,5 @@ The base has a Promoter, use that to get promoted.
         quest = src.quests.questMap["GetPromotion"](targetRank=5)
         return ([quest],None)
 
-
-        # kill snatchers
-        snatcherCount = 4
-        if snatcherCount:
-            quest = src.quests.questMap["SecureTile"](toSecure=(5,6,0),endWhenCleared=False,reason="confront the Snatchers",story="You reach out to your implant and it answers:\n\nThe base itself is safe now, but there are Snatchers out there.\nThey will try to swarm and kill everyone that goes outside.",lifetime=100)
-            return ([quest],None)
-
-        return (None,None)
-
-        room = character.container
-
-        if not isinstance(character.container, src.rooms.Room):
-            quest = src.quests.questMap["GoHome"](description="go to command centre")
-            return  ([quest],None)
-        
-        for item in room.itemsOnFloor:
-            if not item.bolted:
-                continue
-            if item.type != "Promoter":
-                continue
-
-            interactionCommand = "J"
-            if "advancedInteraction" in character.interactionState:
-                interactionCommand = ""
-            if item.getPosition() == (character.xPosition-1,character.yPosition,0):
-                return (None,(interactionCommand+"a","get promotion"))
-            if item.getPosition() == (character.xPosition+1,character.yPosition,0):
-                return (None,(interactionCommand+"d","get promotion"))
-            if item.getPosition() == (character.xPosition,character.yPosition-1,0):
-                return (None,(interactionCommand+"w","get promotion"))
-            if item.getPosition() == (character.xPosition,character.yPosition+1,0):
-                return (None,(interactionCommand+"s","get promotion"))
-            
-            quest = src.quests.questMap["GoToPosition"](targetPosition=item.getPosition(),ignoreEndBlocked=True,description="go to promoter ")
-            return  ([quest],None)
-        
-        quest = src.quests.questMap["GoToTile"](targetPosition=(7,7,0),description="go to command centre")
-        return  ([quest],None)
-
 # register quest type
 src.quests.addType(GetRank5PromotionStory)

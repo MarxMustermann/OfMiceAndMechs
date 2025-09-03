96654a94e11b387a0772eb391069a7511aa42774
MarxMustermann <marxmustermann@riseup.net>
2025-09-03 12:05:00 +0200

    made items boltable

--- src/itemFolder/resources/paving.py
+++ src/itemFolder/resources/paving.py
@@ -19,4 +19,35 @@ class Paving(src.items.Item):
 
         super().__init__(display=(src.interaction.urwid.AttrSpec("#d84", "black"),"::"))
 
+    def getConfigurationOptions(self, character):
+        '''
+        register the configuration options with superclass
+
+        Parameters:
+            character: the character trying to conigure the machine
+        '''
+
+        options = super().getConfigurationOptions(character)
+        if self.bolted:
+            options["b"] = ("unbolt", self.unboltAction)
+        else:
+            options["b"] = ("bolt down", self.boltAction)
+        return options
+
+    def boltAction(self,character):
+        '''
+        bolt the item down
+        '''
+        self.bolted = True
+        character.addMessage("you bolt down the Paving")
+        character.changed("boltedItem",{"character":character,"item":self})
+
+    def unboltAction(self,character):
+        '''
+        unbolt the item
+        '''
+        self.bolted = False
+        character.addMessage("you unbolt the Paving")
+        character.changed("unboltedItem",{"character":character,"item":self})
+
 src.items.addType(Paving)

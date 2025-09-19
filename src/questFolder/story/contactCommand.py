5d930b28ffe65c82a78662aa5c8a02942e0ac9ed
MarxMustermann <marxmustermann@riseup.net>
2025-09-19 03:29:31 +0200

    added quest markers

--- src/questFolder/story/contactCommand.py
+++ src/questFolder/story/contactCommand.py
@@ -132,4 +132,26 @@ Comtact the bases leader to get further instrucions.
             return False
         return False
 
+    def getQuestMarkersSmall(self,character,renderForTile=False):
+        '''
+        return the quest markers for the normal map
+        '''
+        if isinstance(character.container,src.rooms.Room):
+            if renderForTile:
+                return []
+        else:
+            if not renderForTile:
+                return []
+
+        result = super().getQuestMarkersSmall(character,renderForTile=renderForTile)
+        if not renderForTile:
+            if isinstance(character.container,src.rooms.Room):
+                for item in character.container.itemsOnFloor:
+                    if not item.type == "Communicator":
+                        continue
+                    if not item.bolted:
+                        continue
+                    result.append((item.getPosition(),"target"))
+        return result
+
 src.quests.addType(ContactCommand)

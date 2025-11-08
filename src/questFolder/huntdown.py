dec457533e8bd22f361bf24bf14a1d1ae01b3a24
MarxMustermann <marxmustermann@riseup.net>
2025-11-08 21:28:58 +0100

    reduced code depth

--- src/questFolder/huntdown.py
+++ src/questFolder/huntdown.py
@@ -27,51 +27,55 @@ class Huntdown(src.quests.MetaQuestSequence):
     def getNextStep(self,character=None,ignoreCommands=False,dryRun=True):
         self.triggerCompletionCheck()
 
-        if not self.subQuests:
-            if isinstance(character.container, src.rooms.Room):
-                charPos = (character.container.xPosition,character.container.yPosition,0)
+        if self.subQuests:
+            return (None,None)
+
+        if isinstance(character.container, src.rooms.Room):
+            charPos = (character.container.xPosition,character.container.yPosition,0)
+        else:
+            charPos = (character.xPosition//15,character.yPosition//15,0)
+
+        if not self.target.container:
+            return (None,("10.","wait"))
+
+        if isinstance(self.target.container, src.rooms.Room):
+            targetPos = (self.target.container.xPosition,self.target.container.yPosition,0)
+        else:
+            targetPos = (self.target.xPosition//15,self.target.yPosition//15,0)
+
+        if targetPos == (0,0,0):
+            return (None,None)
+
+        if character.yPosition%15 == 0:
+            return (None,("s","move toward target"))
+        if character.yPosition%15 == 14:
+            return (None,("w","move toward target"))
+        if character.xPosition%15 == 0:
+            return (None,("d","move toward target"))
+        if character.xPosition%15 == 14:
+            return (None,("a","move toward target"))
+
+        if not dryRun:
+            self.target.changed("hunted", {"hunter":character})
+
+        if charPos != targetPos:
+            if self.alwaysfollow or abs(charPos[0]-targetPos[0])+abs(charPos[1]-targetPos[1]) == 1:
+                newPos = targetPos
             else:
-                charPos = (character.xPosition//15,character.yPosition//15,0)
+                abort_reason = "target escaped"
+                if not dryRun:
+                    self.fail(abort_reason)                     
+                return (None,("+",f"abort quest\n({abort_reason})"))
 
-            if not self.target.container:
-                return (None,("10.","wait"))
+            if newPos[0] in (0,14,) or newPos[1] in (0,14):
+                abort_reason = "target left terrain"
+                if not dryRun:
+                    self.fail(abort_reason)
+                return (None,("+",f"abort quest\n({abort_reason})"))
 
-            if isinstance(self.target.container, src.rooms.Room):
-                targetPos = (self.target.container.xPosition,self.target.container.yPosition,0)
-            else:
-                targetPos = (self.target.xPosition//15,self.target.yPosition//15,0)
-
-            if targetPos == (0,0,0):
-                return (None,None)
-
-            if character.yPosition%15 == 0:
-                return (None,("s","move toward target"))
-            if character.yPosition%15 == 14:
-                return (None,("w","move toward target"))
-            if character.xPosition%15 == 0:
-                return (None,("d","move toward target"))
-            if character.xPosition%15 == 14:
-                return (None,("a","move toward target"))
-
-            if not dryRun:
-                self.target.changed("hunted", {"hunter":character})
-
-            if charPos != targetPos:
-                if self.alwaysfollow or abs(charPos[0]-targetPos[0])+abs(charPos[1]-targetPos[1]) == 1:
-                    newPos = targetPos
-                else:
-                    if not dryRun:
-                        self.fail()                     
-                    return (None,None)
-
-                if newPos[0] in (0,14,) or newPos[1] in (0,14):
-                    if not dryRun:
-                        self.fail("target left terrain")
-                    return (None,None)
-
-                quest = src.quests.questMap["GoToTile"](paranoid=True,targetPosition=newPos)
-                return ([quest],None)
-            quest = src.quests.questMap["Fight"](suicidal=True)
+            quest = src.quests.questMap["GoToTile"](paranoid=True,targetPosition=newPos)
             return ([quest],None)
-        return (None,None)
+        quest = src.quests.questMap["Fight"](suicidal=True)
+        return ([quest],None)
+
 src.quests.addType(Huntdown)

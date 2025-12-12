ffaf75bcd788bdcd3cec71980b21000c4ef02e38
MarxMustermann <marxmustermann@riseup.net>
2025-12-12 01:35:58 +0100

    made info drop more obvious and easier to understand

--- src/itemFolder/story/memoryFragment.py
+++ src/itemFolder/story/memoryFragment.py
@@ -21,9 +21,11 @@ class MemoryFragment(src.items.Item):
         pos = (random.randint(1,13),random.randint(1,13))
         terrain = src.gamestate.gamestate.terrainMap[pos[1]][pos[0]]
         currentInfo = character.terrainInfo.get(terrain.getPosition())
+        is_new_info = False
         if not currentInfo:
             character.terrainInfo[terrain.getPosition()] = {"tag":terrain.tag}
             text = f"you uncovered the terrain {pos}.\nIt is a {terrain.tag}"
+            is_new_info = True
         else:
             text = f"you already know about the uncover terrain."
 
@@ -35,6 +37,22 @@ class MemoryFragment(src.items.Item):
                 text += f"the memory fragment crystalises."
             else:
                 text += f"the memory fragment starts to crystalise. But there is no mana in this terrain"
+
+        if is_new_info:
+            pseudo_map = []
+            for y in range(0,15):
+                pseudo_map.append(["  "])
+                for x in range(0,15):
+                    if pos == (x,y):
+                        pseudo_map[y].append("XX")
+                    elif y == 7 or x == 7:
+                        pseudo_map[y].append("  ")
+                    elif y in (0,14) or x in (0,14):
+                        pseudo_map[y].append("~~")
+                    else:
+                        pseudo_map[y].append("  ")
+                pseudo_map[y].append("\n")
+            character.showTextMenu([text,"\n\n\n",pseudo_map])
         character.addMessage(text)
 
         # self destruct

a8baba7b620f2d7cead11baa1980492325856db4
MarxMustermann <marxmustermann@riseup.net>
2025-12-19 06:49:32 +0100

    added message

--- src/questFolder/beUsefull.py
+++ src/questFolder/beUsefull.py
@@ -512,6 +512,7 @@ Press d to move the cursor and show the subquests description.
                 print(step)
                 raise Exception("invalid step format")
             if step != (None,None) and not (not step[0] and not step[1]):
+                character.addMessage(f"starting duty: {duty}")
                 if not dryRun:
                     self.numTasksDone += 1
 

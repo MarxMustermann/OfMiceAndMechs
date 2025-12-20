4ee2850dd81e5e5c6f5df0e82aabf7ff856706e5
MarxMustermann <marxmustermann@riseup.net>
2025-12-20 18:16:42 +0100

    simplified code

--- src/itemFolder/resources/wall.py
+++ src/itemFolder/resources/wall.py
@@ -31,14 +31,4 @@ class Wall(src.items.Item):
             options["b"] = ("bolt down", self.boltAction)
         return options
 
-    def boltAction(self,character):
-        self.bolted = True
-        character.addMessage("you bolt down the Wall")
-        character.changed("boltedItem",{"character":character,"item":self})
-
-    def unboltAction(self,character):
-        self.bolted = False
-        character.addMessage("you unbolt the Wall")
-        character.changed("unboltedItem",{"character":character,"item":self})
-
 src.items.addType(Wall)

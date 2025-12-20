aa3e550846d4f168c075b216ebe7a1018006a943
MarxMustermann <marxmustermann@riseup.net>
2025-12-20 18:28:14 +0100

    made item boltable

--- src/itemFolder/artworks/personnelTracker.py
+++ src/itemFolder/artworks/personnelTracker.py
@@ -37,4 +37,19 @@ class PersonnelTracker(src.items.Item):
         #    return
         super().apply(character)
 
+    def getConfigurationOptions(self, character):
+        """
+        register the configuration options with superclass
+
+        Parameters:
+            character: the character trying to conigure the machine
+        """
+
+        options = super().getConfigurationOptions(character)
+        if self.bolted:
+            options["b"] = ("unbolt", self.unboltAction)
+        else:
+            options["b"] = ("bolt down", self.boltAction)
+        return options
+
 src.items.addType(PersonnelTracker)

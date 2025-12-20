c69cbd89e970fe60ab19b9c9baeb604efb512604
MarxMustermann <marxmustermann@riseup.net>
2025-12-20 19:22:47 +0100

    simplified code

--- src/itemFolder/military/rodTower.py
+++ src/itemFolder/military/rodTower.py
@@ -166,14 +166,4 @@ weapon: None
 """
         return result
 
-    def boltAction(self,character):
-        self.bolted = True
-        character.addMessage("you bolt down the ScrapCompactor")
-        character.changed("boltedItem",{"character":character,"item":self})
-
-    def unboltAction(self,character):
-        self.bolted = False
-        character.addMessage("you unbolt the ScrapCompactor")
-        character.changed("unboltedItem",{"character":character,"item":self})
-
 src.items.addType(RodTower)

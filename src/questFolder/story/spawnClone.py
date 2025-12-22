9eec51782932335e99a10dea706d69b30956f7a7
MarxMustermann <marxmustermann@riseup.net>
2025-12-22 18:11:32 +0100

    added quest reasons and comments

--- src/questFolder/story/spawnClone.py
+++ src/questFolder/story/spawnClone.py
@@ -17,23 +17,30 @@ class SpawnClone(src.quests.MetaQuestSequence):
         '''
         try to fix the condition if the quest fails
         '''
+
+        # handle weird edge case
         if extraParam["quest"] not in self.subQuests:
             return
 
+        # remove failed quest
         self.subQuests.remove(extraParam["quest"])
 
+        # set up helper variables
         quest = extraParam["quest"]
 
+        # try to get a goo flask
         reason = extraParam.get("reason")
         if reason == "no source for item GooFlask":
+
+            # go to spawn room (obsolete?)
             if not self.character.getBigPosition() == (7,8,0):
                 newQuest = src.quests.questMap["GoToTile"](targetPosition=(7,8,0),reason="go to spawning room",description="go to spawning room")
                 self.addQuest(newQuest)
                 self.startWatching(newQuest,self.handleQuestFailure,"failed")
                 return
 
+            # pick up goo flask from the environment
             for (coord,itemList) in self.character.getTerrain().itemsByBigCoordinate.items():
-
                 if self.character.getTerrain().getRoomByPosition(coord):
                     continue
                 is_guarded = False
@@ -43,53 +50,57 @@ class SpawnClone(src.quests.MetaQuestSequence):
                         continue
                 if is_guarded:
                     continue
-
                 for item in itemList:
                     if not item.type == "GooFlask":
                         continue
-
-                    newQuest = src.quests.questMap["ScavengeTile"](targetPositionBig=coord,toCollect="GooFlask",tryHard=True)
+                    newQuest = src.quests.questMap["ScavengeTile"](targetPositionBig=coord,toCollect="GooFlask",tryHard=True, reason="pick up goo flasks")
                     self.addQuest(newQuest)
                     self.startWatching(newQuest,self.handleQuestFailure,"failed")
                     return
 
+            # refill flask at goo dispenser
             for room in self.character.getTerrain().rooms:
                 for item in room.getItemsByType("GooDispenser"):
                     if item.charges > 0:
-                        newQuest = src.quests.questMap["FillFlask"]()
+                        newQuest = src.quests.questMap["FillFlask"](reason="have a full goo flask")
                         self.addQuest(newQuest)
                         self.startWatching(newQuest,self.handleQuestFailure,"failed")
-                        newQuest = src.quests.questMap["FetchItems"](toCollect="Flask",tryHard=True,amount=1)
+                        newQuest = src.quests.questMap["FetchItems"](toCollect="Flask",tryHard=True,amount=1,reason="have a flask to fill")
                         self.addQuest(newQuest)
                         self.startWatching(newQuest,self.handleQuestFailure,"failed")
                         return
 
-            # fetch any goo flasks lieing around
+            # fetch any goo flasks lying around
             for room in self.character.getTerrain().rooms:
                 for item in room.getItemsByType("GooFlask"):
-                    newQuest = src.quests.questMap["CleanSpace"](targetPositionBig=room.getPosition(),targetPosition=item.getPosition())
+                    newQuest = src.quests.questMap["CleanSpace"](targetPositionBig=room.getPosition(),targetPosition=item.getPosition(),reason="pick up GooFlask")
                     self.addQuest(newQuest)
                     self.startWatching(newQuest,self.handleQuestFailure,"failed")
                     return
 
+            # produce goo
             if self.character.container.isRoom:
+                
+                # produce goo
                 for item in self.character.container.getItemsByType("GooProducer"):
                     if item.readyToUse():
-                        newQuest = src.quests.questMap["OperateMachine"](targetPosition=item.getPosition(),targetPositionBig=item.getBigPosition())
+                        newQuest = src.quests.questMap["OperateMachine"](targetPosition=item.getPosition(),targetPositionBig=item.getBigPosition(),reason="be able to fill your flask")
                         self.addQuest(newQuest)
                         self.startWatching(newQuest,self.handleQuestFailure,"failed")
                         return
 
+                # produce goo precursor (PressCake)
                 for item in self.character.container.getItemsByType("BioPress"):
                     if item.readyToUse():
-                        newQuest = src.quests.questMap["OperateMachine"](targetPosition=item.getPosition(),targetPositionBig=item.getBigPosition())
+                        newQuest = src.quests.questMap["OperateMachine"](targetPosition=item.getPosition(),targetPositionBig=item.getBigPosition(),reason="be able to produce goo")
                         self.addQuest(newQuest)
                         self.startWatching(newQuest,self.handleQuestFailure,"failed")
                         return
 
+                # produce goo precursor (BioMass)
                 for item in self.character.container.getItemsByType("BloomShredder"):
                     if item.readyToUse():
-                        newQuest = src.quests.questMap["OperateMachine"](targetPosition=item.getPosition(),targetPositionBig=item.getBigPosition())
+                        newQuest = src.quests.questMap["OperateMachine"](targetPosition=item.getPosition(),targetPositionBig=item.getBigPosition(),reason="work towards producing goo")
                         self.addQuest(newQuest)
                         self.startWatching(newQuest,self.handleQuestFailure,"failed")
                         return
@@ -105,9 +116,9 @@ class SpawnClone(src.quests.MetaQuestSequence):
                         drop_position = item.getPosition(offset=(-1,0,0))
                         if not room.getPositionWalkable(drop_position):
                             continue
-                        newQuest = src.quests.questMap["RestockRoom"](toRestock="Bloom",targetPositionBig=room.getPosition(),targetPosition=drop_position)
+                        newQuest = src.quests.questMap["RestockRoom"](toRestock="Bloom",targetPositionBig=room.getPosition(),targetPosition=drop_position,reason="fill the BloomShredders input")
                         self.addQuest(newQuest)
-                        newQuest = src.quests.questMap["FetchItems"](toCollect="Bloom")
+                        newQuest = src.quests.questMap["FetchItems"](toCollect="Bloom",reason="have blooms to feed into the BloomShredder")
                         self.addQuest(newQuest)
                         return
 
@@ -122,11 +133,12 @@ class SpawnClone(src.quests.MetaQuestSequence):
                             continue
                         numItems += 1
                 if numItems > 4:
-                    newQuest = src.quests.questMap["ClearTile"](targetPositionBig=room.getPosition())
+                    newQuest = src.quests.questMap["ClearTile"](targetPositionBig=room.getPosition(),reason="maintain trap rooms")
                     self.addQuest(newQuest)
                     self.startWatching(newQuest,self.handleQuestFailure,"failed")
                     return
 
+            # get the base state
             hasClone = False
             for other_character in self.character.getTerrain().getAllCharacters():
                 if other_character == self.character:
@@ -136,16 +148,15 @@ class SpawnClone(src.quests.MetaQuestSequence):
                 hasClone = True
 
             if hasClone and random.random() < 0.5:
-                newQuest = src.quests.questMap["Adventure"](lifetime=random.random()*10000+2000)
+                newQuest = src.quests.questMap["Adventure"](lifetime=random.random()*10000+2000,reason="find some goo flasks")
                 self.addQuest(newQuest)
                 self.startWatching(newQuest,self.handleQuestFailure,"failed")
                 return
             else: 
-                # farm for blooms
-                newQuest = src.quests.questMap["FarmMold"](tryHard=True,lifetime=1000)
+                newQuest = src.quests.questMap["FarmMold"](tryHard=True,lifetime=1000,reason="obtain some blooms")
                 self.addQuest(newQuest)
                 self.startWatching(newQuest,self.handleQuestFailure,"failed")
-                newQuest = src.quests.questMap["Heal"](noWaitHeal=True)
+                newQuest = src.quests.questMap["Heal"](noWaitHeal=True,reason="be safe while farming")
                 self.addQuest(newQuest)
                 return
 

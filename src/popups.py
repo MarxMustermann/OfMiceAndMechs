fced2d752aa6547b8c9a8b85bf5b708c986a0d4d
MarxMustermann <marxmustermann@riseup.net>
2025-12-23 07:04:12 +0100

    tweaked UI positioning

--- src/popups.py
+++ src/popups.py
@@ -31,7 +31,7 @@ class Popup(ABC):
     @staticmethod
     def open_Popup(character, text, container=None):
         submenue = src.menuFolder.textMenu.TextMenu(
-            text,
+            text+"\n",
             specialKeys={"q": {"container": container, "method": "openQuestMenu", "params": {"character": character}}},
         )
         submenue.tag = "popup"

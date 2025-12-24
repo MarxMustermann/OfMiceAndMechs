549f4af3d88eaa700e422935dbd1c1ad5ceb60a6
MarxMustermann <marxmustermann@riseup.net>
2025-12-24 04:34:50 +0100

    Revert "removed backward compability code"
    This reverts commit 8bb2740e01c69f5428c2251328284256de6a3410.

--- src/interaction.py
+++ src/interaction.py
@@ -4753,7 +4753,10 @@ def renderGameDisplay(renderChar=None):
             pydisplay.fill((0, 0, 0))
             font = pygame.font.Font("config/DejaVuSansMono.ttf", 14)
 
-            plainText = stringifyUrwid(submenue.render())
+            if submenue:
+                plainText = stringifyUrwid(submenue.render())
+            else:
+                plainText = stringifyUrwid(main.get_text())
             plainText = stringifyUrwid(plainText)
             counter = 0
             for line in plainText.split("\n"):
@@ -4763,7 +4766,10 @@ def renderGameDisplay(renderChar=None):
 
             pygame.display.update()
         if tcodConsole:
-            text = submenue.render()
+            if submenue:
+                text = submenue.render()
+            else:
+                text = main.get_text()
             plainText = stringifyUrwid(text)
 
             height = 0

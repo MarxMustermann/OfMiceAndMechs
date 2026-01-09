a7d738c45fbdfdd727ed4d80233d19614f34c31c
MarxMustermann <marxmustermann@riseup.net>
2026-01-09 15:35:50 +0100

    made bonus HP carry over respawn

--- src/cinematicsFolder/death.py
+++ src/cinematicsFolder/death.py
@@ -78,6 +78,9 @@ def Death(extraParam):
         if src.gamestate.gamestate.difficulty == "difficult":
             chosen_candidate.health = int(chosen_candidate.health/2)
             chosen_candidate.maxHealth = int(chosen_candidate.maxHealth/2)
+        if src.gamestate.gamestate.difficulty == "easy":
+            chosen_candidate.health = int(chosen_candidate.health*2)
+            chosen_candidate.maxHealth = int(chosen_candidate.maxHealth*2)
         chosen_candidate.addListener(src.cinematicsFolder.death.Death,"died_pre")
         chosen_candidate.autoExpandQuests = src.gamestate.gamestate.mainChar.autoExpandQuests
         chosen_candidate.autoExpandQuests2 = src.gamestate.gamestate.mainChar.autoExpandQuests2

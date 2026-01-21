21db0d48eee3a7a85c53afff0246fb4221d1bbeb
MarxMustermann <marxmustermann@riseup.net>
2026-01-21 20:55:06 +0100

    increased color difference for shown commands

--- src/menuFolder/questMenu.py
+++ src/menuFolder/questMenu.py
@@ -271,7 +271,7 @@ class QuestMenu(src.subMenu.SubMenu):
                     for letter in solvingCommangString:
                         color = "#fff"
                         if letter.isupper():
-                            color = "#ccd"
+                            color = "#aad"
                         convertedCommanString.append((src.interaction.urwid.AttrSpec(color, "default"), letter))
                         convertedCommanString.append(" ")
                     if solvingCommangString in ("esc","enter","backspace",):

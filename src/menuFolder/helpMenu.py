959190651f08206795c0de471f38fb3acfe59c4e
MarxMustermann <marxmustermann@riseup.net>
2026-02-03 19:44:10 +0100

    added instruction for toggling fullscreen

--- src/menuFolder/helpMenu.py
+++ src/menuFolder/helpMenu.py
@@ -68,6 +68,7 @@ class HelpMenu(src.subMenu.SubMenu):
         txt += " q: open quests\n"
         txt += " Q: open advanced quest menu\n"
         txt += " i: show inventory\n"
+        txt += " F11: toggle fullscreen\n"
         txt += "\n"
         txt += "sadly the controls cannot be changed at the moment\n"
         txt += "if you have issues with the character running into walls, tap the keys instead of holding them\n"

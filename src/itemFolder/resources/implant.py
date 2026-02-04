376be782922730ffb860852351f4886cb79d17a6
MarxMustermann <marxmustermann@riseup.net>
2026-02-04 15:26:17 +0100

    added description

--- src/itemFolder/resources/implant.py
+++ src/itemFolder/resources/implant.py
@@ -3,7 +3,7 @@ import src
 
 class Implant(src.items.Item):
     type = "Implant"
-
+    description = "A mind control implant for Clones"
     def __init__(self):
         super().__init__(display="ip", name="Implant")
         self.walkable = True

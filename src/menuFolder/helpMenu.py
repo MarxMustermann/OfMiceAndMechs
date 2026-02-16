bf544a9ca6abab5743406e4293fcefe8c0de61f0
MarxMustermann <marxmustermann@riseup.net>
2026-02-16 20:45:18 +0100

    made headings be on top

--- src/menuFolder/helpMenu.py
+++ src/menuFolder/helpMenu.py
@@ -59,10 +59,29 @@ class HelpMenu(src.subMenu.SubMenu):
     def render(self):
         txt = []
         txt.append("press w/s to move cursor\n")
+        title = ""
         color = "#666"
         if self.index == 0:
             color = "#fff"
-        txt.append((src.interaction.urwid.AttrSpec(color, "#000"),"\n== implant ==\n"))
+            title = "implant"
+        txt.append((src.interaction.urwid.AttrSpec(color, "#000"),"\n== implant ="))
+        color = "#666"
+        if self.index == 1:
+            color = "#fff"
+            title = "keybindings"
+        txt.append((src.interaction.urwid.AttrSpec(color, "#000"),"\n= keybindings ="))
+        color = "#666"
+        if self.index == 2:
+            color = "#fff"
+            title = "user interface"
+        txt.append((src.interaction.urwid.AttrSpec(color, "#000"),"= user interface ="))
+        color = "#666"
+        if self.index == 3:
+            color = "#fff"
+            title = "submenues"
+        txt.append((src.interaction.urwid.AttrSpec(color, "#000"),"= submenues==\n"))
+
+        txt.append((src.interaction.urwid.AttrSpec("#fff", "#000"),f"\n\n== {title} ==\n"))
         if self.index == 0:
             txt.append("\n")
             txt.append("The implant is your main help in this game.\n")
@@ -72,53 +91,41 @@ class HelpMenu(src.subMenu.SubMenu):
             txt.append("You are very welcome to not do what the implant suggests.\n")
             txt.append("The implants instructions will try to adpapt as good as it can.\n")
 
-        color = "#666"
-        if self.index == 1:
-            color = "#fff"
-        txt.append((src.interaction.urwid.AttrSpec(color, "#000"),"\n== keybindings ==\n"))
         if self.index == 1:
             txt.append("\n  = movement =\n\n")
-            txt.append("      w/a/s/d - move north/east/south/west (up/left/down/right)\n")
-            txt.append("      use shift for special movement\n")
+            txt.append("w/a/s/d - move north/east/south/west (up/left/down/right)\n")
+            txt.append("use shift for special movement\n")
             txt.append("\n  = wait =\n\n")
-            txt.append("      ./:/,/; - wait 1 turn / 0.1 turn / enemy approach / enemy nearby\n")
+            txt.append("./:/,/; - wait 1 turn / 0.1 turn / enemy approach / enemy nearby\n")
             txt.append("\n  = item interaction =\n\n")
-            txt.append("      j/J - activate items\n")
-            txt.append("      c/C - complex activate items\n")
-            txt.append("      k/K - pick up item\n")
-            txt.append("      l/L -  drop item\n")
+            txt.append("j/J - activate items\n")
+            txt.append("c/C - complex activate items\n")
+            txt.append("k/K - pick up item\n")
+            txt.append("l/L -  drop item\n")
             txt.append("\n")
-            txt.append("     lowercase keys work on the square you stand on or the last item you bumped into\n")
-            txt.append("     uppercase keys open a secondary menu for selection what to interact with\n")
+            txt.append("lowercase keys work on the square you stand on or the last item you bumped into\n")
+            txt.append("uppercase keys open a secondary menu for selection what to interact with\n")
             txt.append("\n  = fighting =\n\n")
-            txt.append("      w/a/s/d - attack north/east/south/west\n")
-            txt.append("      use shift for alternate attacks\n")
-            txt.append("      f - shoot\n")
-            txt.append("      m - attack enemy on the same square\n")
-        color = "#666"
-        if self.index == 2:
-            color = "#fff"
-        txt.append((src.interaction.urwid.AttrSpec(color, "#000"),"\n== submenues==\n"))
+            txt.append("w/a/s/d - attack north/east/south/west\n")
+            txt.append("use shift for alternate attacks\n")
+            txt.append("f - shoot\n")
+            txt.append("m - attack enemy on the same square\n")
         if self.index == 2:
             txt.append("\n")
-            txt.append("      o: observe\n")
-            txt.append("      O: observe alternates\n")
-            txt.append("      e/E - examine nearby items\n")
-            txt.append("      q: open quests\n")
-            txt.append("      Q: open advanced quest menu\n")
-            txt.append("      i: open inventory\n")
-            txt.append("      x: open message log\n")
-            txt.append("      v: open character overwiev\n")
-            txt.append("      p: cast magic\n")
+            txt.append("o: observe\n")
+            txt.append("O: observe alternates\n")
+            txt.append("e/E - examine nearby items\n")
+            txt.append("q: open quests\n")
+            txt.append("Q: open advanced quest menu\n")
+            txt.append("i: open inventory\n")
+            txt.append("x: open message log\n")
+            txt.append("v: open character overwiev\n")
+            txt.append("p: cast magic\n")
 
-        color = "#666"
-        if self.index == 3:
-            color = "#fff"
-        txt.append((src.interaction.urwid.AttrSpec(color, "#000"),"\n== user interface ==\n"))
         if self.index == 3:
             txt.append("\n")
-            txt.append("      F11: toggle fullscreen\n")
-            txt.append("      ctrl +/-: zoom in/out\n")
+            txt.append("F11: toggle fullscreen\n")
+            txt.append("ctrl +/-: zoom in/out\n")
             txt.append("\n")
             txt.append("\n")
         txt.append("\n")

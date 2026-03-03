5bc5b7d3e8236795be36d84dc5694c393bac5393
MarxMustermann <marxmustermann@riseup.net>
2026-03-03 01:50:41 +0100

    added more pathways

--- src/story.py
+++ src/story.py
@@ -1924,6 +1924,8 @@ but they are likely to explode when disturbed.
             scrapStorage_room.addWalkingSpace((x,6,0))
         for y in (11,10,9,8,7):
             scrapStorage_room.addWalkingSpace((6,y,0))
+        for pos in [(9,5,0),(9,4,0),(10,4,0),(11,4,0),(11,3,0),(11,2,0),(10,2,0),(9,2,0),(8,2,0),(7,2,0),(7,3,0),(7,4,0),(8,4,0)]:
+            scrapStorage_room.addWalkingSpace(pos)
 
         for x in (1,3,5,):
             for y in (7,8,9,10,11):

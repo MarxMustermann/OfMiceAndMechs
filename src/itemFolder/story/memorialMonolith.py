bdc5dd76cc5ea0e64ddfe1b6901f721f8e12a8d3
MarxMustermann <marxmustermann@riseup.net>
2026-03-07 10:50:35 +0100

    added tile

--- src/itemFolder/story/memorialMonolith.py
+++ src/itemFolder/story/memorialMonolith.py
@@ -1,5 +1,8 @@
 import src
 
+import numpy as np
+
+memorialMonolith_texture = {}
 
 class MemorialMonolith(src.items.Item):
     """
@@ -14,6 +17,29 @@ class MemorialMonolith(src.items.Item):
         self.bolted = True
         self.walkable = False
         self.inscription = inscription
+
+    def drawSDL(self, renderer, basePos, fg_color=(255,255,255,255), bg_color=(0,0,0,255), tileSize=None):
+
+        if tileSize is None:
+            tileSize = src.interaction.tileHeight
+
+        identifier = (fg_color,bg_color)
+        texture = memorialMonolith_texture.get(identifier)
+        if not texture:
+            base_path = "config/tiles/"
+            path = base_path+"MemorialMonolith.png"
+            circle = src.interaction.tcod.image.Image.from_file(path)
+            for x_index in range(0,circle.width):
+                for y_index in range(0,circle.height):
+                    color = circle.get_pixel(x_index,y_index)
+                    if color == (255, 255, 255):
+                        circle.put_pixel(x_index,y_index,fg_color[:3])
+                    if color == (0, 0, 0):
+                        circle.put_pixel(x_index,y_index,bg_color[:3])
+            texture = renderer.upload_texture(np.asarray(circle))
+            memorialMonolith_texture[identifier] = texture
+            print("rebuilding","ScrapCompactor.png",identifier)
+        renderer.copy(texture, (0,0,texture.width,texture.height),(basePos[0],basePos[1],tileSize,tileSize),)
 
     def apply(self, character):
         """

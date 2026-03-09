d3c6d2f0ef7a880bbd22929e00520be2af15e0d8
MarxMustermann <marxmustermann@riseup.net>
2026-03-10 00:06:13 +0100

    made changing sound settings have effect instantly

--- src/menuFolder/settingMenu.py
+++ src/menuFolder/settingMenu.py
@@ -41,6 +41,7 @@ class SettingMenu(src.subMenu.SubMenu):
                 match setting:
                     case "sound":
                         src.interaction.settings["sound"] = 32 if src.interaction.settings["sound"] == 0 else 0
+                        src.interaction.changeVolume()
                     case "set sound volume":
                         src.interaction.settings["sound"] += -1 if key in ("a","left") else +1
                         src.interaction.settings["sound"] = src.helpers.clamp(src.interaction.settings["sound"], 0, 32)

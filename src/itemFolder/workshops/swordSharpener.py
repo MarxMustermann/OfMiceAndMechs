30e72b5d0c223198b9111795ef38e5a4d144505a
MarxMustermann <marxmustermann@riseup.net>
2026-03-14 20:51:05 +0100

    removed obsolete code

--- src/itemFolder/workshops/swordSharpener.py
+++ src/itemFolder/workshops/swordSharpener.py
@@ -27,29 +27,6 @@ class SwordSharpener(src.items.itemMap["WorkShop"]):
                 }
         self.preferredMaxDamage = None
 
-    def drawSDL(self, renderer, basePos, fg_color=(255,255,255,255), bg_color=(0,0,0,255), tileSize=None):
-
-        if tileSize is None:
-            tileSize = src.interaction.tileHeight
-
-        identifier = (fg_color,bg_color)
-        texture = swordSharpener_texture.get(identifier)
-        if not texture:
-            base_path = "config/tiles/"
-            path = base_path+"SwordSharpener.png"
-            circle = src.interaction.tcod.image.Image.from_file(path)
-            for x_index in range(0,circle.width):
-                for y_index in range(0,circle.height):
-                    color = circle.get_pixel(x_index,y_index)
-                    if color == (255, 255, 255):
-                        circle.put_pixel(x_index,y_index,fg_color[:3])
-                    if color == (0, 0, 0):
-                        circle.put_pixel(x_index,y_index,bg_color[:3])
-            texture = renderer.upload_texture(np.asarray(circle))
-            swordSharpener_texture[identifier] = texture
-            print("rebuilding","SwordSharpener.png",identifier)
-        renderer.copy(texture, (0,0,texture.width,texture.height),(basePos[0],basePos[1],tileSize,tileSize),)
-
     def sharpenEquippedSwordHook(self, character):
         '''
         calls the actual sharpinging function with converted parameters

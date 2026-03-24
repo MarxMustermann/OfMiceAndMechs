e738545a0da012868ec94ba750e28ba04274c66b
MarxMustermann <marxmustermann@riseup.net>
2026-03-24 22:39:09 +0100

    made enemies react instantly

--- src/questFolder/secureTile.py
+++ src/questFolder/secureTile.py
@@ -179,6 +179,7 @@ Use simple attacks only.
                     y= src.helpers.clamp(y+int(random.uniform(-3,3)),2,11)
                     quest = src.quests.questMap["GoToPosition"](targetPosition = (x,y),reason="get to a nicer spot",idleMovement=True)
                     return ([quest], None)
+                return (None, (".","wait"))
                 if not character.rank and character.charType == "Clone":
                     return (None, ("....","wait"))
                 else:

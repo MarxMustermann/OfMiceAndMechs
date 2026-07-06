8bdb63979031c15d87ed2e053a087a07b3ffb264
MarxMustermann <marxmustermann@riseup.net>
2026-07-06 02:18:39 +0200

    added walkingspace to connect doors

--- src/story.py
+++ src/story.py
@@ -2123,7 +2123,10 @@ sure i'll produce equipment for you as long as you bring me the raw material.
         # add anvil area
         walkingSpace.extend([
                 (5,4,0),(4,4,0),(3,4,0),(2,4,0),(1,4,0),(1,3,0),(1,2,0),
-                (2,2,0),(2,1,0),(3,1,0),(4,1,0),(4,2,0),(5,2,0),(5,3,0)
+                (2,2,0),(2,1,0),(3,1,0),(4,1,0),(4,2,0),(5,2,0),(5,3,0),
+                (6,4,0),(6,3,0),(6,2,0),(6,1,0),
+                (4,6,0),(3,6,0),(2,6,0),(1,6,0),
+                (8,6,0),(9,6,0),(10,6,0),(11,6,0),
             ])
         for x in range(2,5,):
             baseCoreRoom.addStorageSlot((x,5,0),"Scrap",{"desiredState":"filled"})

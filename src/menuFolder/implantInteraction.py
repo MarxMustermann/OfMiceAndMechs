a5f925e5e1f8f73d53405cc707468e93a834ec68
MarxMustermann <marxmustermann@riseup.net>
2026-07-26 19:20:49 +0200

    dead code

--- src/menuFolder/implantInteraction.py
+++ src/menuFolder/implantInteraction.py
@@ -532,7 +532,6 @@ It will start to rebuild its working area.
 """,(src.interaction.urwid.AttrSpec(src.interaction.disabled_ui_color,"black"),"""Shall i assign you a quest?"""),"""
 """]
             options = []
-            #options.append(("improve","improve your equipment"))
             shown_heal = False
             last_heal = src.gamestate.gamestate.stern.get("last_heal",0)
             if character.health < character.adjustedMaxHealth//2 and last_heal < src.gamestate.gamestate.tick-500:

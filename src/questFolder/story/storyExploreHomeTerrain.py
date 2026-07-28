57a47db3a6350080ebef2e9c0e29a7549c0dcc5a
MarxMustermann <marxmustermann@riseup.net>
2026-07-28 13:45:23 +0200

    prevented looting functionally empty rooms

--- src/questFolder/story/storyExploreHomeTerrain.py
+++ src/questFolder/story/storyExploreHomeTerrain.py
@@ -31,7 +31,7 @@ class StoryExploreHomeTerrain(src.quests.MetaQuestSequence):
             
             if enemies:
                 continue
-            if not room.getItems(needsUnbolted=True):
+            if not room.getItems(needsUnbolted=True,ignoreBigItems=True):
                 continue
 
             if room.getPosition() not in result and room.getPosition() not in self.donePointsOfInterest:

c05fb85551576c1eeeda283605f1763374d591ad
MarxMustermann <marxmustermann@riseup.net>
2026-07-29 16:27:55 +0200

    fixed bug

--- src/questFolder/industry/buildRoom.py
+++ src/questFolder/industry/buildRoom.py
@@ -366,7 +366,7 @@ If something disturbs you, destroy it.
             baseNeighbours = []
             offsets = ((0,1,0),(1,0,0),(0,-1,0),(-1,0,0))
             for room in terrain.rooms:
-                if room.tag.lower() in ("shelter","traproom","entryroom","trapsupport","ruin",):
+                if room.tag and room.tag.lower() in ("shelter","traproom","entryroom","trapsupport","ruin",):
                     continue
                 pos = room.getPosition()
                 for offset in offsets:

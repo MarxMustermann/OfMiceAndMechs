83fa2ce12544bfd02ed45e3bbbd75a605ffe3ad9
MarxMustermann <marxmustermann@riseup.net>
2026-08-02 22:35:17 +0200

    reduced useless walking

--- src/menuFolder/implantInteraction.py
+++ src/menuFolder/implantInteraction.py
@@ -124,7 +124,8 @@ class ImplantInteraction(src.menues.SubMenu):
                     if selection == "explore":
                         quests.append(src.quests.questMap["StoryExploreHomeTerrain"](lifetime=500))
                     if selection == "getweapon":
-                        quests.append(src.quests.questMap["ClearInventory"]())
+                        if not character.getFreeInventorySpace():
+                            quests.append(src.quests.questMap["ClearInventory"](returnToTile=False))
                         quests.append(src.quests.questMap["GoToTile"](targetPosition=(7,5,0)))
                         quests.append(src.quests.questMap["Scavenge"](toCollect="Rod",amountToCollect=1,ignoreAlarm=True))
                         quests.append(src.quests.questMap["Equip"]())

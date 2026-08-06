71a5bdddc3f961d5f70a604b528dc71d7cb69425
MarxMustermann <marxmustermann@riseup.net>
2026-08-06 13:04:10 +0200

    made quest failure messages less agressive

--- src/quests.py
+++ src/quests.py
@@ -349,7 +349,7 @@ class Quest:
     """
     def fail(self,reason=None):
         self.changed("failed",{"reason":reason,"quest":self})
-        if reason and self.character and not reason == "abort":
+        if reason and self.character and not reason in ("abort","canceled",):
             self.character.notify(f"failed quest {self.description},\nbecause of {reason}")
         if self.failTrigger:
             self.failTrigger()

6570ca399aa0927385c061403314321c468f21d4
MarxMustermann <marxmustermann@riseup.net>
2026-08-11 19:48:37 +0200

    added another NPC to wake

--- src/story.py
+++ src/story.py
@@ -3065,6 +3065,12 @@ sure i'll produce equipment for you as long as you bring me the raw material.
             painterRoom.addWalkingSpace((6,y,0))
         for x in range(1,6):
             painterRoom.addWalkingSpace((x,6,0))
+        stasisTank = src.items.itemMap["StasisTank"]()
+        painting_npc = src.magic.createBurnedInClone("painting",homeTerrain=currentTerrain,homePosition=(7,4,0),faction=faction)
+        stasisTank.character = painting_npc
+        painterRoom.addItem(stasisTank,(9,3,0))
+        for pos in [(6,5,0),(6,4,0),(6,3,0),(7,3,0),(8,3,0),(9,4,0),(9,5,0),(9,6,0),(8,6,0),(7,6,0)]:
+            painterRoom.addWalkingSpace(pos)
 
         # add rod room
         lootRoom = architect.doAddRoom(

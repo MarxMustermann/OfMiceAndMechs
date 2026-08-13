c204621f3a14b538a5d87b47555cdcd9e20600dc
MarxMustermann <marxmustermann@riseup.net>
2026-08-14 01:10:06 +0200

    made menu scrollable

--- src/menuFolder/characterStatsMenu.py
+++ src/menuFolder/characterStatsMenu.py
@@ -8,12 +8,24 @@ class CharacterStatsMenu(src.menues.SubMenu):
     def __init__(self,character):
         self.type = "CharacterStatsMenu"
         self.character = character
+        self.offset = 0
         super().__init__()
 
     def handleKey(self, key, noRender=False, character=None):
 
+        # change offset
+        if key == "a":
+            self.offset -= 1
+            if self.offset < 0:
+                self.offset = 0
+        if key == "d":
+            self.offset += 1
+
         # exit submenu
-        return key == "esc"
+        if key == "esc":
+            self.done = True
+            return True
+        return False
 
     def getTitle(self):
         return "CHARACTER STATISTICS"
@@ -21,11 +33,20 @@ class CharacterStatsMenu(src.menues.SubMenu):
     def text(self, character):
         text = "Character Statistics:\n\n"
 
+        counter = 0
+        if self.offset:
+            text += f"{self.offset} entries skipped\n\n"
         for stat_name in character.stats:
             stat = character.stats[stat_name]
             if isinstance(stat, int):
+                counter += 1
+                if counter <= self.offset:
+                    continue
                 text += f"{stat_name}: {stat}\n"
             elif isinstance(stat, dict):
+                if counter+len(stat) <= self.offset:
+                    counter += len(stat)
+                    continue
                 stat_sum = sum(stat.values())
                 text += f"{stat_name}: {stat_sum}\n"
                 if len(stat):
@@ -36,6 +57,9 @@ class CharacterStatsMenu(src.menues.SubMenu):
                             max_length = len(inner_name)
                     amount_to_number = max_length + 1
                     for inner_stat_name in stat:
+                        counter += 1
+                        if counter <= self.offset:
+                            continue
                         inner_name = self.beautify(inner_stat_name)
                         text += (
                             f" {inner_name}:"

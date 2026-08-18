422494a48defdaf900c297d94dd5039d29825ad9
MarxMustermann <marxmustermann@riseup.net>
2026-08-19 01:53:04 +0200

    removed obsolete code

--- src/itemFolder/military/alarmBell.py
+++ src/itemFolder/military/alarmBell.py
@@ -32,19 +32,4 @@ class AlarmBell(src.items.Item):
             if character:
                 character.addMessage("you enable the alarm")
 
-    def getConfigurationOptions(self, character):
-        """
-        register the configuration options with superclass
-
-        Parameters:
-            character: the character trying to conigure the machine
-        """
-
-        options = super().getConfigurationOptions(character)
-        if self.bolted:
-            options["b"] = ("unbolt", self.unboltAction)
-        else:
-            options["b"] = ("bolt down", self.boltAction)
-        return options
-
 src.items.addType(AlarmBell)

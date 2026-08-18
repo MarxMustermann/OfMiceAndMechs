1d8d2ea35240f4a08c81d4bbbdb471039ea4ec84
MarxMustermann <marxmustermann@riseup.net>
2026-08-19 01:44:50 +0200

    removed obsolete code

--- src/itemFolder/story/monsterSpawner.py
+++ src/itemFolder/story/monsterSpawner.py
@@ -76,36 +76,5 @@ strength: {self.strength}
 """
         return text
 
-    def getConfigurationOptions(self, character):
-        '''
-        register the configuration options with superclass
-
-        Parameters:
-            character: the character trying to conigure the machine
-        '''
-
-        options = super().getConfigurationOptions(character)
-        if self.bolted:
-            options["b"] = ("unbolt", self.unboltAction)
-        else:
-            options["b"] = ("bolt down", self.boltAction)
-        return options
-
-    def boltAction(self,character):
-        '''
-        bolt the item down
-        '''
-        self.bolted = True
-        character.addMessage("you bolt down the MonsterSpawner")
-        character.changed("boltedItem",{"character":character,"item":self})
-
-    def unboltAction(self,character):
-        '''
-        unbolt the item
-        '''
-        self.bolted = False
-        character.addMessage("you unbolt the MonsterSpawner")
-        character.changed("unboltedItem",{"character":character,"item":self})
-
 # register the item
 src.items.addType(MonsterSpawner)
